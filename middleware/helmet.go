c29b4c576f180353d0b70e605192d9601048cb66
Fenny <fenny@protonmail.com>
2020-02-04 13:55:18 +0100

    .

--- middleware/helmet.go
+++ middleware/helmet.go
@@ -3,12 +3,12 @@ package middleware
 import (
 	"fmt"
 
-	"github.com/fenny/fiber"
+	"github.com/gofiber/fiber"
 )
 
 // Helmet : Helps secure your apps by setting various HTTP headers.
-func Helmet() func(*fiber.Midware) {
-	return func(c *fiber.Midware) {
+func Helmet() func(*fiber.Ctx) {
+	return func(c *fiber.Ctx) {
 		fmt.Println("Helmet is still under development, this middleware does nothing yet.")
 		c.Next()
 	}
