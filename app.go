bf66978a7f39cb2f8abe5efd65519652c9a0551b
Fenny <fenny@protonmail.com>
2020-02-22 17:03:30 -0500

    Fix comments

--- app.go
+++ app.go
@@ -1,4 +1,6 @@
-// https://fiber.wiki
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://fiber.wiki
+// 📝 Github Repository: https://github.com/gofiber/fiber
 
 package fiber
 
