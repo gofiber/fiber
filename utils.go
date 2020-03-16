7eed1953a3e0309474a48f23063d770d7212039e
Fenny <25108519+Fenny@users.noreply.github.com>
2020-03-16 22:29:32 +0100

    Add faster comparison util

--- utils.go
+++ utils.go
@@ -39,6 +39,15 @@ func groupPaths(prefix, path string) string {
 	return path
 }
 
+// BenchmarkDefault    322504144    18.6 ns/op // string == string
+// BenchmarkisEqual    353663168    17.0 ns/op // isEqual(string, string)
+func isEqual(a, b string) bool {
+	if len(a) == len(b) {
+		return a == b
+	}
+	return false
+}
+
 func getParams(path string) (params []string) {
 	if len(path) < 1 {
 		return
