5e36e6b11a329a36733ea779e1fcc39864a654f7
Fenny <25108519+Fenny@users.noreply.github.com>
2020-05-11 17:49:59 +0200

    Add utils tests

--- utils_test.go
+++ utils_test.go
@@ -50,33 +50,87 @@ func Test_Utils_getMIME(t *testing.T) {
 // 	// TODO
 // }
 
-// func Test_Utils_getString(t *testing.T) {
-// 	// TODO
-// }
+func Test_Utils_getString(t *testing.T) {
+	res := getString([]byte("Hello, World!"))
+	assertEqual(t, "Hello, World!", res)
 
-// func Test_Utils_getStringImmutable(t *testing.T) {
-// 	// TODO
-// }
+	res = getString([]byte(""))
+	assertEqual(t, "", res)
+}
 
-// func Test_Utils_getBytes(t *testing.T) {
-// 	// TODO
-// }
+func Test_Utils_getStringImmutable(t *testing.T) {
+	res := getStringImmutable([]byte("Hello, World!"))
+	assertEqual(t, "Hello, World!", res)
 
-// func Test_Utils_getBytesImmutable(t *testing.T) {
-// 	// TODO
-// }
+	res = getStringImmutable([]byte(""))
+	assertEqual(t, "", res)
+}
 
-// func Test_Utils_methodINT(t *testing.T) {
-// 	// TODO
-// }
+func Test_Utils_getBytes(t *testing.T) {
+	res := getBytes("Hello, World!")
+	assertEqual(t, []byte("Hello, World!"), res)
 
-// func Test_Utils_statusMessage(t *testing.T) {
-// 	// TODO
-// }
+	res = getBytes("")
+	assertEqual(t, []byte{}, res)
+}
 
-// func Test_Utils_extensionMIME(t *testing.T) {
-// 	// TODO
-// }
+func Test_Utils_getBytesImmutable(t *testing.T) {
+	res := getBytesImmutable("Hello, World!")
+	assertEqual(t, []byte("Hello, World!"), res)
+
+	res = getBytesImmutable("")
+	assertEqual(t, []byte{}, res)
+}
+
+func Test_Utils_methodINT(t *testing.T) {
+	res := methodINT[MethodGet]
+	assertEqual(t, 0, res)
+	res = methodINT[MethodHead]
+	assertEqual(t, 1, res)
+	res = methodINT[MethodPost]
+	assertEqual(t, 2, res)
+	res = methodINT[MethodPut]
+	assertEqual(t, 3, res)
+	res = methodINT[MethodPatch]
+	assertEqual(t, 4, res)
+	res = methodINT[MethodDelete]
+	assertEqual(t, 5, res)
+	res = methodINT[MethodConnect]
+	assertEqual(t, 6, res)
+	res = methodINT[MethodOptions]
+	assertEqual(t, 7, res)
+	res = methodINT[MethodTrace]
+	assertEqual(t, 8, res)
+}
+
+func Test_Utils_statusMessage(t *testing.T) {
+	res := statusMessage[102]
+	assertEqual(t, "Processing", res)
+
+	res = statusMessage[303]
+	assertEqual(t, "See Other", res)
+
+	res = statusMessage[404]
+	assertEqual(t, "Not Found", res)
+
+	res = statusMessage[507]
+	assertEqual(t, "Insufficient Storage", res)
+
+}
+
+func Test_Utils_extensionMIME(t *testing.T) {
+	res := extensionMIME[".html"]
+	assertEqual(t, "text/html", res)
+
+	res = extensionMIME["html"]
+	assertEqual(t, "text/html", res)
+
+	res = extensionMIME[".msp"]
+	assertEqual(t, "application/octet-stream", res)
+
+	res = extensionMIME["msp"]
+	assertEqual(t, "application/octet-stream", res)
+}
 
 // func Test_Utils_getParams(t *testing.T) {
 // 	// TODO
