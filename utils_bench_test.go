c89ea5b26d769ee1ff3dbe3406fcbe5f36d0d462
Fenny <25108519+Fenny@users.noreply.github.com>
2020-05-16 00:26:45 +0200

    Update utils_bench_test.go

--- utils_bench_test.go
+++ utils_bench_test.go
@@ -72,10 +72,6 @@ func Benchmark_Utils_extensionMIME(b *testing.B) {
 // 	// TODO
 // }
 
-// func Benchmark_Utils_matchParams(b *testing.B) {
-// 	// TODO
-// }
-
 func Benchmark_Utils_getTrimmedParam(b *testing.B) {
 	var res string
 	for n := 0; n < b.N; n++ {
