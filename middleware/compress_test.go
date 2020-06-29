d7dcf528cad099880e0a7ad4c949535a09304513
Fenny <25108519+Fenny@users.noreply.github.com>
2020-06-30 00:50:50 +0200

    Add mw benchmarks

--- middleware/compress_test.go
+++ middleware/compress_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/utils"
+	"github.com/valyala/fasthttp"
 )
 
 // go test -run Test_Middleware_Compress
@@ -28,4 +29,23 @@ func Test_Middleware_Compress(t *testing.T) {
 	utils.AssertEqual(t, "gzip", resp.Header.Get(fiber.HeaderContentEncoding))
 	utils.AssertEqual(t, fiber.MIMETextPlainCharsetUTF8, resp.Header.Get(fiber.HeaderContentType))
 	os.Remove("../ctx.go.fiber.gz")
+}
+
+// go test -v -run=^$ -bench=Benchmark_Middleware_Compress -benchmem -count=4
+func Benchmark_Middleware_Compress(b *testing.B) {
+	app := fiber.New()
+	app.Use(Compress())
+	app.Get("/", func(c *fiber.Ctx) {
+		c.SendFile("../ctx.go", true)
+	})
+	handler := app.Handler()
+
+	c := &fasthttp.RequestCtx{}
+	c.Request.SetRequestURI("/")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		handler(c)
+	}
 }
