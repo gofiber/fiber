d3039680fea190b406e4ee65e89483e05a982bca
Fenny <25108519+Fenny@users.noreply.github.com>
2020-07-10 14:01:52 +0200

    🚀 Bump version v1.12.6

--- app.go
+++ app.go
@@ -34,7 +34,7 @@ import (
 )
 
 // Version of current package
-const Version = "1.12.5"
+const Version = "1.12.6"
 
 // Map is a shortcut for map[string]interface{}, useful for JSON returns
 type Map map[string]interface{}
