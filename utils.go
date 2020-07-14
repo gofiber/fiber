d61e4785780fb97df19e611cc9d01f73655a3d50
renanbastos93 <renanbastos.tec@gmail.com>
2020-07-13 23:41:37 -0300

    :rocket: refactor: improve logic method BodyParser

--- utils.go
+++ utils.go
@@ -300,6 +300,7 @@ const (
 	MIMETextHTMLCharsetUTF8              = "text/html; charset=utf-8"
 	MIMETextPlainCharsetUTF8             = "text/plain; charset=utf-8"
 	MIMEApplicationXMLCharsetUTF8        = "application/xml; charset=utf-8"
+	MIMEApplicationJSONCharsetUTF8       = "application/json; charset=utf-8"
 	MIMEApplicationJavaScriptCharsetUTF8 = "application/javascript; charset=utf-8"
 )
 
