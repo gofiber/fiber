2ce81c18359ab7328dd58ca76c6be7fa509faa5d
Fenny <25108519+Fenny@users.noreply.github.com>
2020-08-11 00:02:00 +0200

    ⚙ init server only once

--- app.go
+++ app.go
@@ -442,9 +442,7 @@ func (app *App) Serve(ln net.Listener, tlsconfig ...*tls.Config) error {
 // To use Prefork, please use app.Listen()
 func (app *App) Listener(ln net.Listener, tlsconfig ...*tls.Config) error {
 	// Update server settings
-	if !app.initialized {
-		app.init()
-	}
+	app.init()
 	// TLS config
 	if len(tlsconfig) > 0 {
 		ln = tls.NewListener(ln, tlsconfig[0])
@@ -477,9 +475,7 @@ func (app *App) Listen(address interface{}, tlsconfig ...*tls.Config) error {
 		addr = ":" + addr
 	}
 	// Update server settings
-	if !app.initialized {
-		app.init()
-	}
+	app.init()
 	// Start prefork
 	if app.Settings.Prefork {
 		return app.prefork(addr, tlsconfig...)
@@ -508,9 +504,7 @@ func (app *App) Listen(address interface{}, tlsconfig ...*tls.Config) error {
 
 // Handler returns the server handler.
 func (app *App) Handler() fasthttp.RequestHandler {
-	if !app.initialized {
-		app.init()
-	}
+	app.init()
 	return app.handler
 }
 
@@ -552,9 +546,7 @@ func (app *App) Test(request *http.Request, msTimeout ...int) (*http.Response, e
 		return nil, err
 	}
 	// Update server settings
-	if !app.initialized {
-		app.init()
-	}
+	app.init()
 	// Create test connection
 	conn := new(testConn)
 	// Write raw http request
@@ -600,9 +592,11 @@ func (dl *disableLogger) Printf(format string, args ...interface{}) {
 }
 
 func (app *App) init() *App {
+	// Initialize only once
+	if app.initialized {
+		return app
+	}
 	app.mutex.Lock()
-	// Only init once
-	app.initialized = true
 	// Load view engine if provided
 	if app.Settings != nil {
 		// Only load templates if an view engine is specified
@@ -649,6 +643,8 @@ func (app *App) init() *App {
 	app.server.ReadBufferSize = app.Settings.ReadBufferSize
 	app.server.WriteBufferSize = app.Settings.WriteBufferSize
 	app.buildTree()
+	// App is initialized
+	app.initialized = true
 	app.mutex.Unlock()
 	return app
 }
