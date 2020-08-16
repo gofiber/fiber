a25ef4793888ddb252a719eb1085bf9a73e2576a
Fenny <25108519+Fenny@users.noreply.github.com>
2020-08-16 14:45:28 +0200

    middleware

--- recover/recover.go
+++ recover/recover.go
@@ -6,17 +6,45 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-// New will create a recover handler that recovers from panics and passes them to the global error handler
-func New() fiber.Handler {
+// Config defines the config for middleware.
+type Config struct {
+	// Next defines a function to skip this middleware when returned true.
+	Next func(c *fiber.Ctx) bool
+}
+
+// ConfigDefault is the default config
+var ConfigDefault = Config{
+	Next: nil,
+}
+
+// New creates a new middleware handler
+func New(config ...Config) fiber.Handler {
+	// Set default config
+	cfg := ConfigDefault
+
+	// Override config if provided
+	if len(config) > 0 {
+		cfg = config[0]
+
+		if cfg.Next == nil {
+			cfg.Next = ConfigDefault.Next
+		}
+	}
+
+	// Return new handler
 	return func(c *fiber.Ctx) (err error) {
+		// Catch panics
 		defer func() {
 			if r := recover(); r != nil {
 				var ok bool
 				if err, ok = r.(error); !ok {
+					// Set error that will call the global error handler
 					err = fmt.Errorf("%v", r)
 				}
 			}
 		}()
+
+		// Return err if exist, else move to next handler
 		return c.Next()
 	}
 }
--- requestid/requestid.go
+++ requestid/requestid.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gofiber/utils"
 )
 
-// Config defines the config for requestid middleware.
+// Config defines the config for middleware.
 type Config struct {
 	// Next defines a function to skip this middleware when returned true.
 	Next func(c *fiber.Ctx) bool
@@ -37,16 +37,17 @@ func New(config ...Config) fiber.Handler {
 
 	// Override config if provided
 	if len(config) > 0 {
-		if config[0].Next == nil {
-			config[0].Next = ConfigDefault.Next
-		}
-		if config[0].Header == "" {
-			config[0].Header = ConfigDefault.Header
-		}
-		if config[0].Generator == nil {
-			config[0].Generator = ConfigDefault.Generator
-		}
 		cfg = config[0]
+
+		if cfg.Next == nil {
+			cfg.Next = ConfigDefault.Next
+		}
+		if cfg.Header == "" {
+			cfg.Header = ConfigDefault.Header
+		}
+		if cfg.Generator == nil {
+			cfg.Generator = ConfigDefault.Generator
+		}
 	}
 
 	// Return new handler
@@ -55,13 +56,8 @@ func New(config ...Config) fiber.Handler {
 		if cfg.Next != nil && cfg.Next(c) {
 			return c.Next()
 		}
-		// Get id from request
-		rid := c.Get(cfg.Header)
-
-		// Create new UUID if empty
-		if len(rid) <= 0 {
-			rid = cfg.Generator()
-		}
+		// Get id from request, else we generate one
+		rid := c.Get(cfg.Header, cfg.Generator())
 
 		// Set new id to response header
 		c.Set(cfg.Header, rid)
