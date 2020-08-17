81e4627093630e5a526f6a08382dd9cae7c85125
Fenny <25108519+Fenny@users.noreply.github.com>
2020-08-17 12:36:09 +0200

    📦 Compress

---
+++ compress/compress.go
@@ -0,0 +1,96 @@
+package compress
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/valyala/fasthttp"
+)
+
+// Config defines the config for middleware.
+type Config struct {
+	// Next defines a function to skip this middleware when returned true.
+	Next func(c *fiber.Ctx) bool
+
+	// CompressLevel determins the compression algoritm
+	// Optional. Default: LevelDisabled
+	// LevelDisabled:			-1
+	// LevelDefault:			0
+	// LevelBestSpeed:			1
+	// LevelBestCompression:	2
+	Level int
+}
+
+// ConfigDefault is the default config
+var ConfigDefault = Config{
+	Next:  nil,
+	Level: LevelDefault,
+}
+
+// Compression levels
+const (
+	LevelDisabled        = -1
+	LevelDefault         = 0
+	LevelBestSpeed       = 1
+	LevelBestCompression = 2
+)
+
+// New creates a new middleware handler
+func New(config ...Config) fiber.Handler {
+	// Set default config
+	cfg := ConfigDefault
+
+	// Override config if provided
+	if len(config) > 0 {
+		cfg = config[0]
+
+		// Set default values
+		if cfg.Next == nil {
+			cfg.Next = ConfigDefault.Next
+		}
+		if cfg.Level < -1 || cfg.Level > 2 {
+			cfg.Level = ConfigDefault.Level
+		}
+	}
+
+	// Setup request handlers
+	var (
+		fctx       fasthttp.RequestHandler
+		compressor fasthttp.RequestHandler
+	)
+
+	// Setup compression algorithm
+	switch cfg.Level {
+	case 0:
+		// LevelDefault
+		compressor = fasthttp.CompressHandlerBrotliLevel(fctx, fasthttp.CompressBrotliDefaultCompression, fasthttp.CompressDefaultCompression)
+	case 1:
+		// LevelBestSpeed
+		compressor = fasthttp.CompressHandlerBrotliLevel(fctx, fasthttp.CompressBrotliBestSpeed, fasthttp.CompressBestSpeed)
+	case 2:
+		// LevelBestCompression
+		compressor = fasthttp.CompressHandlerBrotliLevel(fctx, fasthttp.CompressBrotliBestCompression, fasthttp.CompressBestCompression)
+	default:
+		// LevelDisabled
+		return func(c *fiber.Ctx) error {
+			return c.Next()
+		}
+	}
+
+	// Return new handler
+	return func(c *fiber.Ctx) error {
+		// Don't execute the middleware if Next returns true
+		if cfg.Next != nil && cfg.Next(c) {
+			return c.Next()
+		}
+
+		// Continue stack
+		if err := c.Next(); err != nil {
+			return err
+		}
+
+		// Compress response
+		compressor(c.Fasthttp())
+
+		// Return from handler
+		return nil
+	}
+}
