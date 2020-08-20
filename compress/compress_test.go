3546a2b96fec38198298c9cc5c9c70a95a3ef6cf
Fenny <25108519+Fenny@users.noreply.github.com>
2020-08-20 16:55:53 +0200

    🧪 add compress unit test

--- compress/compress_test.go
+++ compress/compress_test.go
@@ -1,6 +1,7 @@
 package compress
 
 import (
+	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 
@@ -8,22 +9,67 @@ import (
 	"github.com/gofiber/utils"
 )
 
+var filedata []byte
+
+func init() {
+	dat, err := ioutil.ReadFile("../.github/testdata/fs/img/fiber.png")
+	if err != nil {
+		panic(err)
+	}
+	filedata = dat
+}
+
 // go test -run Test_Compress
-func Test_Compress(t *testing.T) {
+func Test_Compress_Gzip(t *testing.T) {
 	app := fiber.New()
 
 	app.Use(New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendFile("../filesystem/data/img/map.png", true)
+		return c.Send(filedata)
 	})
 
 	req := httptest.NewRequest("GET", "/", nil)
-	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Encoding", "gzip")
 
 	resp, err := app.Test(req)
 	utils.AssertEqual(t, nil, err, "app.Test(req)")
 	utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
-	utils.AssertEqual(t, "image/png", resp.Header.Get(fiber.HeaderContentType))
 	utils.AssertEqual(t, "gzip", resp.Header.Get(fiber.HeaderContentEncoding))
+}
+
+func Test_Compress_Deflate(t *testing.T) {
+	app := fiber.New()
+
+	app.Use(New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Send(filedata)
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Accept-Encoding", "deflate")
+
+	resp, err := app.Test(req)
+	utils.AssertEqual(t, nil, err, "app.Test(req)")
+	utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
+	utils.AssertEqual(t, "deflate", resp.Header.Get(fiber.HeaderContentEncoding))
+}
+
+func Test_Compress_Brotli(t *testing.T) {
+	app := fiber.New()
+
+	app.Use(New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Send(filedata)
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Accept-Encoding", "br")
+
+	resp, err := app.Test(req)
+	utils.AssertEqual(t, nil, err, "app.Test(req)")
+	utils.AssertEqual(t, 200, resp.StatusCode, "Status code")
+	utils.AssertEqual(t, "br", resp.Header.Get(fiber.HeaderContentEncoding))
 }
