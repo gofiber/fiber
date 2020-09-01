f1a9d65c100b14975918981e1d7f5af16993f46a
Fenny <fenny@gofiber.io>
2020-09-01 20:13:29 +0200

    🎈 prepare for v1.15.0

--- app.go
+++ app.go
@@ -32,7 +32,7 @@ import (
 )
 
 // Version of current package
-const Version = "1.14.5"
+const Version = "1.14.5-pre"
 
 // Map is a shortcut for map[string]interface{}, useful for JSON returns
 type Map map[string]interface{}
