872a78042be1c639314b52dd8321875c6f23cd3f
Fenny <25108519+Fenny@users.noreply.github.com>
2020-09-19 11:47:43 +0200

    📦 v2.0.2

--- app.go
+++ app.go
@@ -31,7 +31,7 @@ import (
 )
 
 // Version of current fiber package
-const Version = "2.0.1"
+const Version = "2.0.2"
 
 // Map is a shortcut for map[string]interface{}, useful for JSON returns
 type Map map[string]interface{}
