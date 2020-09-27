d9ca77a834f27cd3f467972d8c6c81b4ffed5242
Fenny <25108519+Fenny@users.noreply.github.com>
2020-09-27 12:23:46 +0200

    ✏ fix typo

--- utils/convert.go
+++ utils/convert.go
@@ -33,28 +33,11 @@ func SafeString(s string) string {
 	return string(UnsafeBytes(s))
 }
 
-// SafeBytes copies a slive to make it immutable
+// SafeBytes copies a slice to make it immutable
 func SafeBytes(b []byte) []byte {
 	tmp := make([]byte, len(b))
 	copy(tmp, b)
 	return tmp
-}
-
-// #nosec G103
-// GetString returns a string pointer without allocation
-func GetString(b []byte) string {
-	return UnsafeString(b)
-}
-
-// #nosec G103
-// GetBytes returns a byte pointer without allocation
-func GetBytes(s string) []byte {
-	return UnsafeBytes(s)
-}
-
-// ImmutableString copies a string to make it immutable
-func ImmutableString(s string) string {
-	return SafeString(s)
 }
 
 const (
@@ -99,4 +82,23 @@ func ByteSize(bytes uint64) string {
 	result := strconv.FormatFloat(value, 'f', 1, 64)
 	result = strings.TrimSuffix(result, ".0")
 	return result + unit
+}
+
+// Deprecated fn's
+
+// #nosec G103
+// GetString returns a string pointer without allocation
+func GetString(b []byte) string {
+	return UnsafeString(b)
+}
+
+// #nosec G103
+// GetBytes returns a byte pointer without allocation
+func GetBytes(s string) []byte {
+	return UnsafeBytes(s)
+}
+
+// ImmutableString copies a string to make it immutable
+func ImmutableString(s string) string {
+	return SafeString(s)
 }
