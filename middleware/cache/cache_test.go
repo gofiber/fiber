f921efe05af4758497ca131a9a4973b2fc6516d7
Konstantinos Lypitkas <klipitkas@gmail.com>
2020-09-30 23:34:31 +0300

    Add basic test for cache middleware

--- middleware/cache/cache_test.go
+++ middleware/cache/cache_test.go
@@ -1,4 +1,41 @@
 // Special thanks to @codemicro for moving this to fiber core
 // Original middleware: github.com/codemicro/fiber-cache
+
 package cache
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+func Test_Cache_GET(t *testing.T) {
+	app := fiber.New()
+
+	app.Use(New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		now := fmt.Sprintf("%d", time.Now().UnixNano())
+		return c.SendString(now)
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	resp, err := app.Test(req)
+	utils.AssertEqual(t, nil, err)
+
+	req = httptest.NewRequest("GET", "/", nil)
+	cachedResp, err := app.Test(req)
+	utils.AssertEqual(t, nil, err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	utils.AssertEqual(t, nil, err)
+	cachedBody, err := ioutil.ReadAll(cachedResp.Body)
+	utils.AssertEqual(t, nil, err)
+
+	utils.AssertEqual(t, cachedBody, body)
+}
