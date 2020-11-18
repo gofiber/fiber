a98fb7a5f81b1dbbd35a52d04269474c69b84199
Fenny <25108519+Fenny@users.noreply.github.com>
2020-11-18 08:57:27 +0100

    🩹 allow to disable BodySize
    Co-Authored-By: Kirill Danshin <9026581+kirillDanshin@users.noreply.github.com>

--- internal/storage/memory/memory.go
+++ internal/storage/memory/memory.go
@@ -14,8 +14,9 @@ type Storage struct {
 	done       chan struct{}
 }
 
-// Common storage errors
-var ErrNotExist = errors.New("key does not exist")
+
+// ErrNotFound means that a get call did not find the requested key.
+var ErrNotFound = errors.New("key not found")
 
 type entry struct {
 	data   []byte
