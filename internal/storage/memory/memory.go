6ded6377126a955d6b52ff8fb457be386782aa70
Fenny <25108519+Fenny@users.noreply.github.com>
2020-11-18 09:02:42 +0100

    🩹 update store logic

--- internal/storage/memory/memory.go
+++ internal/storage/memory/memory.go
@@ -17,6 +17,7 @@ type Storage struct {
 
 // ErrNotFound means that a get call did not find the requested key.
 var ErrNotFound = errors.New("key not found")
+var ErrKeyNotExist = ErrNotFound
 
 type entry struct {
 	data   []byte
