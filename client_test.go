03ec163e2104e0f59d1d99488f1560175dac6360
Muhammed Efe Çetin <efectn@protonmail.com>
2021-10-06 21:26:56 +0300

    Fix tests.

--- client_test.go
+++ client_test.go
@@ -1039,7 +1039,7 @@ func Test_Client_Agent_Struct(t *testing.T) {
 		utils.AssertEqual(t, StatusOK, code)
 		utils.AssertEqual(t, `{"success"`, string(body))
 		utils.AssertEqual(t, 1, len(errs))
-		utils.AssertEqual(t, "json: unexpected end of JSON input after object field key: ", errs[0].Error())
+		utils.AssertEqual(t, "expected colon after object key", errs[0].Error())
 	})
 }
 
