59e4bf6cc57f78fe85734bb7555a6a46787fab16
Jason McNeil <sixcolors@mac.com>
2021-12-28 21:13:20 -0400

    🔧 fix(middleware/csrf): unmatched token returns nil error (#1667)
    * Update csrf.go
    
    * Update csrf_test.go
    
    * fix(middleware/csrf): missing token return and unit test
    
    * Update csrf_test.go

--- middleware/csrf/csrf_test.go
+++ middleware/csrf/csrf_test.go
@@ -240,6 +240,7 @@ func Test_CSRF_ErrorHandler_InvalidToken(t *testing.T) {
 	app := fiber.New()
 
 	errHandler := func(ctx *fiber.Ctx, err error) error {
+		utils.AssertEqual(t, errTokenNotFound, err)
 		return ctx.Status(419).Send([]byte("invalid CSRF token"))
 	}
 
@@ -270,6 +271,7 @@ func Test_CSRF_ErrorHandler_EmptyToken(t *testing.T) {
 	app := fiber.New()
 
 	errHandler := func(ctx *fiber.Ctx, err error) error {
+		utils.AssertEqual(t, errMissingHeader, err)
 		return ctx.Status(419).Send([]byte("empty CSRF token"))
 	}
 
