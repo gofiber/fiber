ad1a925d38188d33c44b80c6a720c7a9d1e921cf
M. Efe Çetin <efectn@protonmail.com>
2022-02-06 23:28:02 +0300

    add missing json errors. (#1755)

--- error.go
+++ error.go
@@ -1,6 +1,9 @@
 package fiber
 
-import "github.com/gofiber/fiber/v2/internal/schema"
+import (
+	"github.com/gofiber/fiber/v2/internal/go-json/errors"
+	"github.com/gofiber/fiber/v2/internal/schema"
+)
 
 type (
 	// Conversion error exposes the internal schema.ConversionError for public use.
@@ -11,4 +14,26 @@ type (
 	EmptyFieldError = schema.EmptyFieldError
 	// MultiError error exposes the internal schema.MultiError for public use.
 	MultiError = schema.MultiError
+)
+
+type (
+	// An InvalidUnmarshalError describes an invalid argument passed to Unmarshal.
+	// (The argument to Unmarshal must be a non-nil pointer.)
+	InvalidUnmarshalError = errors.InvalidUnmarshalError
+
+	// A MarshalerError represents an error from calling a MarshalJSON or MarshalText method.
+	MarshalerError = errors.MarshalerError
+
+	// A SyntaxError is a description of a JSON syntax error.
+	SyntaxError = errors.SyntaxError
+
+	// An UnmarshalTypeError describes a JSON value that was
+	// not appropriate for a value of a specific Go type.
+	UnmarshalTypeError = errors.UnmarshalTypeError
+
+	// An UnsupportedTypeError is returned by Marshal when attempting
+	// to encode an unsupported value type.
+	UnsupportedTypeError = errors.UnsupportedTypeError
+
+	UnsupportedValueError = errors.UnsupportedValueError
 )
