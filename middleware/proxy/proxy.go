0c06985d7e06ae154cef22ae18984141ef5e9f28
bigflood <pyotaesu@gmail.com>
2022-02-12 01:45:21 +0900

    Fix: incorrect scheme on proxy.Do (#1762)

--- middleware/proxy/proxy.go
+++ middleware/proxy/proxy.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"net/url"
@@ -121,10 +122,25 @@ func Do(c *fiber.Ctx, addr string) error {
 	req := c.Request()
 	res := c.Response()
 	req.SetRequestURI(addr)
+	// NOTE: if req.isTLS is true, SetRequestURI keeps the scheme as https.
+	// issue reference:
+	// https://github.com/gofiber/fiber/issues/1762
+	if scheme := getScheme(utils.UnsafeBytes(addr)); len(scheme) > 0 {
+		req.URI().SetSchemeBytes(scheme)
+	}
+
 	req.Header.Del(fiber.HeaderConnection)
 	if err := client.Do(req, res); err != nil {
 		return err
 	}
 	res.Header.Del(fiber.HeaderConnection)
 	return nil
+}
+
+func getScheme(uri []byte) []byte {
+	i := bytes.IndexByte(uri, '/')
+	if i < 1 || uri[i-1] != ':' || i == len(uri)-1 || uri[i+1] != '/' {
+		return nil
+	}
+	return uri[:i-1]
 }
