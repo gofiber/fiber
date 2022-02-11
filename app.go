232c877b50918b8d67ddf47af9a9209ac387ce29
Eren BALCI <erenbalci3939@gmail.com>
2022-02-11 22:49:14 +0300

    fix: backwards incompatible change to fiber.Error

--- app.go
+++ app.go
@@ -84,8 +84,9 @@ type ErrorHandler = func(*Ctx, error) error
 
 // Error represents an error that occurred while handling a request.
 type Error struct {
-	Code    int         `json:"code"`
-	Message interface{} `json:"message"`
+	Code       int         `json:"code"`
+	Message    string      `json:"message"`
+	AnyMessage interface{} `json:"-"`
 }
 
 // App denotes the Fiber application.
@@ -722,31 +723,35 @@ func (app *App) Route(prefix string, fn func(router Router), name ...string) Rou
 
 // Error makes it compatible with the `error` interface.
 func (e *Error) Error() string {
-	return fmt.Sprint(e.Message)
+	return e.Message
 }
 
 // NewError creates a new Error instance with an optional message
 func NewError(code int, message ...interface{}) *Error {
-	e := &Error{
-		Code:    code,
-		Message: utils.StatusMessage(code),
+	err := &Error{
+		Code:       code,
+		Message:    utils.StatusMessage(code),
+		AnyMessage: utils.StatusMessage(code),
 	}
 	if len(message) > 0 {
-		e.Message = message[0]
+		err.Message = utils.ToString(message[0])
+		err.AnyMessage = message[0]
 	}
-	return e
+	return err
 }
 
 // NewErrors creates multiple new Error messages
 func NewErrors(code int, messages ...interface{}) *Error {
-	e := &Error{
-		Code:    code,
-		Message: utils.StatusMessage(code),
+	err := &Error{
+		Code:       code,
+		Message:    utils.StatusMessage(code),
+		AnyMessage: utils.StatusMessage(code),
 	}
 	if len(messages) > 0 {
-		e.Message = messages
+		err.Message = utils.ToString(messages)
+		err.AnyMessage = messages
 	}
-	return e
+	return err
 }
 
 // Listener can be used to pass a custom listener.
--- app_test.go
+++ app_test.go
@@ -1230,15 +1230,15 @@ func Benchmark_App_ETag_Weak(b *testing.B) {
 
 // go test -run Test_NewError
 func Test_NewError(t *testing.T) {
-	e := NewError(StatusForbidden, "permission denied")
-	utils.AssertEqual(t, StatusForbidden, e.Code)
-	utils.AssertEqual(t, "permission denied", fmt.Sprint(e.Message))
+	err := NewError(StatusForbidden, "permission denied")
+	utils.AssertEqual(t, StatusForbidden, err.Code)
+	utils.AssertEqual(t, "permission denied", err.Message)
 }
 
 func Test_NewErrors(t *testing.T) {
-	e := NewErrors(StatusBadRequest, "error 1", "error 2")
-	messages := e.Message.([]interface{})
-	utils.AssertEqual(t, StatusBadRequest, e.Code)
+	err := NewErrors(StatusBadRequest, "error 1", "error 2")
+	messages := err.AnyMessage.([]interface{})
+	utils.AssertEqual(t, StatusBadRequest, err.Code)
 	utils.AssertEqual(t, "error 1", fmt.Sprint(messages[0]))
 	utils.AssertEqual(t, "error 2", fmt.Sprint(messages[1]))
 }
