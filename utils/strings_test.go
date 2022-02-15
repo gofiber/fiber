b0c7f6daba73d44f091b68810b2daf38b882f374
Fufu <fufuok@qq.com>
2022-02-15 10:05:08 +0800

    test: complete test cases for all Trim functions

--- utils/strings_test.go
+++ utils/strings_test.go
@@ -71,6 +71,15 @@ func Test_TrimRight(t *testing.T) {
 
 	res = TrimRight("/test", '/')
 	AssertEqual(t, "/test", res)
+
+	res = TrimRight(" ", ' ')
+	AssertEqual(t, "", res)
+
+	res = TrimRight("  ", ' ')
+	AssertEqual(t, "", res)
+
+	res = TrimRight("", ' ')
+	AssertEqual(t, "", res)
 }
 
 func Benchmark_TrimRight(b *testing.B) {
@@ -97,6 +106,15 @@ func Test_TrimLeft(t *testing.T) {
 
 	res = TrimLeft("test/", '/')
 	AssertEqual(t, "test/", res)
+
+	res = TrimLeft(" ", ' ')
+	AssertEqual(t, "", res)
+
+	res = TrimLeft("  ", ' ')
+	AssertEqual(t, "", res)
+
+	res = TrimLeft("", ' ')
+	AssertEqual(t, "", res)
 }
 
 func Benchmark_TrimLeft(b *testing.B) {
