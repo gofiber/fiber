b0dd0bdb14d05eea303eaee71936ca6cef57b629
M. Efe Çetin <efectn@protonmail.com>
2022-02-22 10:10:55 +0300

    Update app.go (#1787)

--- app.go
+++ app.go
@@ -48,13 +48,13 @@ type Map map[string]interface{}
 // providers
 type Storage interface {
 	// Get gets the value for the given key.
-	// It returns ErrNotFound if the storage does not contain the key.
+	// `nil, nil` is returned when the key does not exist
 	Get(key string) ([]byte, error)
 
-	// Set stores the given value for the given key along with a
-	// time-to-live expiration value, 0 means live for ever
+	// Set stores the given value for the given key along
+	// with an expiration value, 0 means no expiration.
 	// Empty key or value will be ignored without an error.
-	Set(key string, val []byte, ttl time.Duration) error
+	Set(key string, val []byte, exp time.Duration) error
 
 	// Delete deletes the value for the given key.
 	// It returns no error if the storage does not contain the key,
