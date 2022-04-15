d8c672fd7e19ed83076b8810797e29d2ce2849b5
M. Efe Çetin <efectn@protonmail.com>
2022-04-15 15:20:45 +0300

    Update ctx.go

--- ctx.go
+++ ctx.go
@@ -1362,7 +1362,7 @@ func (c *Ctx) SendFileWithConfig(file string, config ...SendFile) error {
 		}
 		sendFileFS.CacheDuration = config[0].CacheDuration
 		sendFileFS.Compress = config[0].Compress
-		if config[0].Compress {
+		if !config[0].Compress {
 			// https://github.com/valyala/fasthttp/blob/master/fs.go#L46
 			c.fasthttp.Request.Header.Del(HeaderAcceptEncoding)
 		}
