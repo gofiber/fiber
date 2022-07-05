744e4da3ef6f934cd0acc87a1486d6f5daecb1b8
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2022-07-05 16:04:32 +0430

    add some parameters to config for supporting #1936 (#1956)
    * add some parameters to config for supporting #1936
    
    * remove go:embed
    
    * apply code review  - 1956#issuecomment-1169811117
    
    * lint
    
    Co-authored-by: Muhammed Efe Çetin <efectn@protonmail.com>

--- middleware/monitor/index.go
+++ middleware/monitor/index.go
@@ -6,25 +6,37 @@ import (
 	"time"
 )
 
-// returns index with new title/refresh
-func newIndex(title string, refresh time.Duration) string {
+type viewBag struct {
+	title      string
+	refresh    time.Duration
+	fontUrl    string
+	chartJsUrl string
+	customHead string
+}
 
-	timeout := refresh.Milliseconds() - timeoutDiff
+// returns index with new title/refresh
+func newIndex(dat viewBag) string {
+
+	timeout := dat.refresh.Milliseconds() - timeoutDiff
 	if timeout < timeoutDiff {
 		timeout = timeoutDiff
 	}
 	ts := strconv.FormatInt(timeout, 10)
-
-	index := strings.ReplaceAll(indexHtml, "$TITLE", title)
-	return strings.ReplaceAll(index, "$TIMEOUT", ts)
+	replacer := strings.NewReplacer("$TITLE", dat.title, "$TIMEOUT", ts,
+		"$FONT_URL", dat.fontUrl, "$CHART_JS_URL", dat.chartJsUrl, "$CUSTOM_HEAD", dat.customHead,
+	)
+	return replacer.Replace(indexHtml)
 }
 
 const (
 	defaultTitle = "Fiber Monitor"
 
-	defaultRefresh = 3 * time.Second
-	timeoutDiff    = 200 // timeout will be Refresh (in millisconds) - timeoutDiff
-	minRefresh     = timeoutDiff * time.Millisecond
+	defaultRefresh    = 3 * time.Second
+	timeoutDiff       = 200 // timeout will be Refresh (in milliseconds) - timeoutDiff
+	minRefresh        = timeoutDiff * time.Millisecond
+	defaultFontURL    = `https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap`
+	defaultChartJsURL = `https://cdn.jsdelivr.net/npm/chart.js@2.9/dist/Chart.bundle.min.js`
+	defaultCustomHead = ``
 
 	// parametrized by $TITLE and $TIMEOUT
 	indexHtml = `<!DOCTYPE html>
@@ -32,8 +44,9 @@ const (
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9/dist/Chart.bundle.min.js"></script>
+	<link href="$FONT_URL" rel="stylesheet">
+	<script src="$CHART_JS_URL"></script>
+
 	<title>$TITLE</title>
 <style>
 	body {
@@ -80,6 +93,7 @@ const (
 		width: 200px;
 		height: 180px;
 	}
+$CUSTOM_HEAD
 </style>
 </head>
 <body>
@@ -253,5 +267,6 @@ const (
 	fetchJSON()
 </script>
 </body>
-</html>`
+</html>
+`
 )
