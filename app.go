f01f0f9f9ad760393d51528b214a1332f8f1fa81
Muhammed Efe Çetin <efectn@protonmail.com>
2022-07-13 18:15:03 +0300

    :sparkles: feature: customizable colors

--- app.go
+++ app.go
@@ -423,47 +423,47 @@ type Static struct {
 type Colors struct {
 	// Black color.
 	//
-	// Optional. Default: \u001b[90m
+	// Optional. Default: "\u001b[90m"
 	Black string
 
 	// Red color.
 	//
-	// Optional. Default: \u001b[91m
+	// Optional. Default: "\u001b[91m"
 	Red string
 
 	// Green color.
 	//
-	// Optional. Default: \u001b[92m
+	// Optional. Default: "\u001b[92m"
 	Green string
 
 	// Yellow color.
 	//
-	// Optional. Default: \u001b[93m
+	// Optional. Default: "\u001b[93m"
 	Yellow string
 
 	// Blue color.
 	//
-	// Optional. Default: \u001b[94m
+	// Optional. Default: "\u001b[94m"
 	Blue string
 
 	// Magenta color.
 	//
-	// Optional. Default: \u001b[95m
+	// Optional. Default: "\u001b[95m"
 	Magenta string
 
 	// Cyan color.
 	//
-	// Optional. Default: \u001b[96m
+	// Optional. Default: "\u001b[96m"
 	Cyan string
 
 	// White color.
 	//
-	// Optional. Default: \u001b[97m
+	// Optional. Default: "\u001b[97m"
 	White string
 
 	// Reset color.
 	//
-	// Optional. Default: \u001b[0m
+	// Optional. Default: "\u001b[0m"
 	Reset string
 }
 
