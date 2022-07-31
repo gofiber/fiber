720546c61667fd7719cb5d9bf843dc754ef4282d
Muhammed Efe Çetin <efectn@protonmail.com>
2022-07-31 14:24:35 +0300

    Fix Benchmark_Router_Github_API

--- router_test.go
+++ router_test.go
@@ -770,9 +770,14 @@ func Benchmark_Router_Github_API(b *testing.B) {
 		c.Request.Header.SetMethod(routesFixture.TestRoutes[i].Method)
 		for n := 0; n < b.N; n++ {
 			c.URI().SetPath(routesFixture.TestRoutes[i].Path)
-			ctx := app.NewCtx(c)
-			match, err = app.next(ctx.(CustomCtx))
+
+			ctx := app.AcquireCtx().(CustomCtx)
+			ctx.Reset(c)
+
+			match, err = app.next(ctx)
+			app.ReleaseCtx(ctx)
 		}
+
 		utils.AssertEqual(b, nil, err)
 		utils.AssertEqual(b, true, match)
 	}
