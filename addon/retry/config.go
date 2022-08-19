4adda508b0a51717c17a2226bf48ca79caff98bc
Gökhan Özeloğlu <33967642+gozeloglu@users.noreply.github.com>
2022-08-19 09:20:14 +0300

    ✨ v3 (feature): add retry mechanism (#1972)
    * v3-retry-mechanism: Add retry mechanism
    
    * General logic is implemented.
    
    * Unit tests are added.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Refactor test assertion
    
    * Replaced testify/assert with fiber's assert.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add test for next method
    
    * currentInterval bug is fixed in Retry.
    
    * If condition is fixed in next.
    
    * struct definition refactored and if condtion is removed in TestExponentialBackoff_Retry.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add config for retry.
    
    * Constant variables are removed.
    
    * Helper function is added for default.
    
    * Helper function is used in New function.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Replace math/rand with crypto/rand
    
    * Random number generation package has been replaced with more secure one,
    crypto/rand.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add a README for retry middleware
    
    * Explanation and examples are added.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Add comment line for documentation
    
    * Comment lines are added for ExponentialBackoff variables.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * Run go mod tidy
    
    * Unused package(s) removed.
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    
    * move middleware -> addon
    
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
    Co-authored-by: Muhammed Efe Çetin <efectn@protonmail.com>

---
+++ addon/retry/config.go
@@ -0,0 +1,66 @@
+package retry
+
+import "time"
+
+// Config defines the config for addon.
+type Config struct {
+	// InitialInterval defines the initial time interval for backoff algorithm.
+	//
+	// Optional. Default: 1 * time.Second
+	InitialInterval time.Duration
+
+	// MaxBackoffTime defines maximum time duration for backoff algorithm. When
+	// the algorithm is reached this time, rest of the retries will be maximum
+	// 32 seconds.
+	//
+	// Optional. Default: 32 * time.Second
+	MaxBackoffTime time.Duration
+
+	// Multiplier defines multiplier number of the backoff algorithm.
+	//
+	// Optional. Default: 2.0
+	Multiplier float64
+
+	// MaxRetryCount defines maximum retry count for the backoff algorithm.
+	//
+	// Optional. Default: 10
+	MaxRetryCount int
+
+	// currentInterval tracks the current waiting time.
+	//
+	// Optional. Default: 1 * time.Second
+	currentInterval time.Duration
+}
+
+// DefaultConfig is the default config for retry.
+var DefaultConfig = Config{
+	InitialInterval: 1 * time.Second,
+	MaxBackoffTime:  32 * time.Second,
+	Multiplier:      2.0,
+	MaxRetryCount:   10,
+	currentInterval: 1 * time.Second,
+}
+
+// configDefault sets the config values if they are not set.
+func configDefault(config ...Config) Config {
+	if len(config) == 0 {
+		return DefaultConfig
+	}
+	cfg := config[0]
+	if cfg.InitialInterval == 0 {
+		cfg.InitialInterval = DefaultConfig.InitialInterval
+	}
+	if cfg.MaxBackoffTime == 0 {
+		cfg.MaxBackoffTime = DefaultConfig.MaxBackoffTime
+	}
+	if cfg.Multiplier <= 0 {
+		cfg.Multiplier = DefaultConfig.Multiplier
+	}
+	if cfg.MaxRetryCount <= 0 {
+		cfg.MaxRetryCount = DefaultConfig.MaxRetryCount
+	}
+	if cfg.currentInterval != cfg.InitialInterval {
+		cfg.currentInterval = DefaultConfig.currentInterval
+	}
+	return cfg
+}
