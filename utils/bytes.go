71637c135c34a5dca7804e0960b17ebe57ab72be
Trim21 <trim21.me@gmail.com>
2022-08-20 07:49:02 +0800

    comment

--- utils/bytes.go
+++ utils/bytes.go
@@ -4,7 +4,7 @@
 
 package utils
 
-// ToLowerBytes converts ascii slice to lower-case
+// ToLowerBytes converts ascii slice to lower-case in-place.
 func ToLowerBytes(b []byte) []byte {
 	for i := 0; i < len(b); i++ {
 		b[i] = toLowerTable[b[i]]
@@ -12,7 +12,7 @@ func ToLowerBytes(b []byte) []byte {
 	return b
 }
 
-// ToUpperBytes converts ascii slice to upper-case
+// ToUpperBytes converts ascii slice to upper-case in-place.
 func ToUpperBytes(b []byte) []byte {
 	for i := 0; i < len(b); i++ {
 		b[i] = toUpperTable[b[i]]
