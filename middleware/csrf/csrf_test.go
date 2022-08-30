6180fd4b819cf796675f5d94ba97eda47d2dc543
René Werner <rene@gofiber.io>
2022-08-30 13:01:11 +0200

    🐛 [Bug]: Strange CSRF middleware behavior with header KeyLookup configuration #2045

--- middleware/csrf/csrf_test.go
+++ middleware/csrf/csrf_test.go
@@ -1,8 +1,6 @@
 package csrf
 
 import (
-	"fmt"
-	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
@@ -343,55 +341,56 @@ func Test_CSRF_ErrorHandler_EmptyToken(t *testing.T) {
 	utils.AssertEqual(t, "empty CSRF token", string(ctx.Response.Body()))
 }
 
-func Test_CSRF_UnsafeHeaderValue(t *testing.T) { // TODO: add the flow of the bug to a testcase before we start fixing it
-	app := fiber.New()
-
-	app.Use(New())
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendStatus(fiber.StatusOK)
-	})
-	app.Get("/test", func(c *fiber.Ctx) error {
-		return c.SendStatus(fiber.StatusOK)
-	})
-	app.Post("/", func(c *fiber.Ctx) error {
-		return c.SendStatus(fiber.StatusOK)
-	})
-
-	resp, err := app.Test(httptest.NewRequest(http.MethodGet, "/", nil))
-	utils.AssertEqual(t, nil, err)
-	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
-
-	var token string
-	for _, c := range resp.Cookies() {
-		if c.Name != ConfigDefault.CookieName {
-			continue
-		}
-		token = c.Value
-		break
-	}
-
-	fmt.Println("token", token)
-
-	getReq := httptest.NewRequest(http.MethodGet, "/", nil)
-	getReq.Header.Set(HeaderName, token)
-	resp, err = app.Test(getReq)
-
-	getReq = httptest.NewRequest(http.MethodGet, "/test", nil)
-	getReq.Header.Set("X-Requested-With", "XMLHttpRequest")
-	getReq.Header.Set(fiber.HeaderCacheControl, "no")
-	getReq.Header.Set(HeaderName, token)
-
-	resp, err = app.Test(getReq)
-
-	getReq.Header.Set(fiber.HeaderAccept, "*/*")
-	getReq.Header.Del(HeaderName)
-	resp, err = app.Test(getReq)
-
-	postReq := httptest.NewRequest(http.MethodPost, "/", nil)
-	postReq.Header.Set("X-Requested-With", "XMLHttpRequest")
-	postReq.Header.Set(HeaderName, token)
-	resp, err = app.Test(postReq)
-}
+// TODO: use this test case and make the unsafe header value bug from https://github.com/gofiber/fiber/issues/2045 reproducible and permanently fixed/tested by this testcase
+//func Test_CSRF_UnsafeHeaderValue(t *testing.T) {
+//	app := fiber.New()
+//
+//	app.Use(New())
+//	app.Get("/", func(c *fiber.Ctx) error {
+//		return c.SendStatus(fiber.StatusOK)
+//	})
+//	app.Get("/test", func(c *fiber.Ctx) error {
+//		return c.SendStatus(fiber.StatusOK)
+//	})
+//	app.Post("/", func(c *fiber.Ctx) error {
+//		return c.SendStatus(fiber.StatusOK)
+//	})
+//
+//	resp, err := app.Test(httptest.NewRequest(http.MethodGet, "/", nil))
+//	utils.AssertEqual(t, nil, err)
+//	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+//
+//	var token string
+//	for _, c := range resp.Cookies() {
+//		if c.Name != ConfigDefault.CookieName {
+//			continue
+//		}
+//		token = c.Value
+//		break
+//	}
+//
+//	fmt.Println("token", token)
+//
+//	getReq := httptest.NewRequest(http.MethodGet, "/", nil)
+//	getReq.Header.Set(HeaderName, token)
+//	resp, err = app.Test(getReq)
+//
+//	getReq = httptest.NewRequest(http.MethodGet, "/test", nil)
+//	getReq.Header.Set("X-Requested-With", "XMLHttpRequest")
+//	getReq.Header.Set(fiber.HeaderCacheControl, "no")
+//	getReq.Header.Set(HeaderName, token)
+//
+//	resp, err = app.Test(getReq)
+//
+//	getReq.Header.Set(fiber.HeaderAccept, "*/*")
+//	getReq.Header.Del(HeaderName)
+//	resp, err = app.Test(getReq)
+//
+//	postReq := httptest.NewRequest(http.MethodPost, "/", nil)
+//	postReq.Header.Set("X-Requested-With", "XMLHttpRequest")
+//	postReq.Header.Set(HeaderName, token)
+//	resp, err = app.Test(postReq)
+//}
 
 // go test -v -run=^$ -bench=Benchmark_Middleware_CSRF_Check -benchmem -count=4
 func Benchmark_Middleware_CSRF_Check(b *testing.B) {
