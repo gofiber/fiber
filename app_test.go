d53b24bf3077d72ce2ce39270ef812194d207db4
Eren BALCI <erenbalci3939@gmail.com>
2022-09-03 12:59:57 +0300

    ✅ test: add Route() func test

--- app_test.go
+++ app_test.go
@@ -649,6 +649,37 @@ func Test_App_Methods(t *testing.T) {
 	testStatus200(t, app, "/john/doe", MethodGet)
 }
 
+func Test_App_Route(t *testing.T) {
+	dummyHandler := testEmptyHandler
+
+	app := New()
+
+	app.Route("/:john?/:doe?").
+		Connect(dummyHandler).
+		Put(dummyHandler).
+		Post(dummyHandler).
+		Delete(dummyHandler).
+		Head(dummyHandler).
+		Patch(dummyHandler).
+		Options(dummyHandler).
+		Trace(dummyHandler).
+		Get(dummyHandler).
+		All(dummyHandler).
+		Use(dummyHandler)
+
+	testStatus200(t, app, "/john/doe", "CONNECT")
+	testStatus200(t, app, "/john/doe", MethodPut)
+	testStatus200(t, app, "/john/doe", MethodPost)
+	testStatus200(t, app, "/john/doe", MethodDelete)
+	testStatus200(t, app, "/john/doe", MethodHead)
+	testStatus200(t, app, "/john/doe", MethodPatch)
+	testStatus200(t, app, "/john/doe", MethodOptions)
+	testStatus200(t, app, "/john/doe", MethodTrace)
+	testStatus200(t, app, "/john/doe", MethodGet)
+	testStatus200(t, app, "/john/doe", MethodPost)
+	testStatus200(t, app, "/john/doe", MethodGet)
+}
+
 // func Test_App_Route_Naming(t *testing.T) {
 // 	app := New()
 // 	handler := func(c *Ctx) error {
