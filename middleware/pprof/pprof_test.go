13247206ab63de182748c23d277675b574ff7c95
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-11-08 22:59:33 -0800

    Add URL prefix to pprof middleware (#2194)
    * Add URL prefix to pprof middleware
    
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
    
    * Minor tweak
    
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
    
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- middleware/pprof/pprof_test.go
+++ middleware/pprof/pprof_test.go
@@ -28,6 +28,24 @@ func Test_Non_Pprof_Path(t *testing.T) {
 	utils.AssertEqual(t, "escaped", string(b))
 }
 
+func Test_Non_Pprof_Path_WithPrefix(t *testing.T) {
+	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+
+	app.Use(New(Config{Prefix: "/federated-fiber"}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("escaped")
+	})
+
+	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 200, resp.StatusCode)
+
+	b, err := io.ReadAll(resp.Body)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, "escaped", string(b))
+}
+
 func Test_Pprof_Index(t *testing.T) {
 	app := fiber.New(fiber.Config{DisableStartupMessage: true})
 
@@ -38,6 +56,25 @@ func Test_Pprof_Index(t *testing.T) {
 	})
 
 	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/debug/pprof/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 200, resp.StatusCode)
+	utils.AssertEqual(t, fiber.MIMETextHTMLCharsetUTF8, resp.Header.Get(fiber.HeaderContentType))
+
+	b, err := io.ReadAll(resp.Body)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, true, bytes.Contains(b, []byte("<title>/debug/pprof/</title>")))
+}
+
+func Test_Pprof_Index_WithPrefix(t *testing.T) {
+	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+
+	app.Use(New(Config{Prefix: "/federated-fiber"}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("escaped")
+	})
+
+	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/federated-fiber/debug/pprof/", nil))
 	utils.AssertEqual(t, nil, err)
 	utils.AssertEqual(t, 200, resp.StatusCode)
 	utils.AssertEqual(t, fiber.MIMETextHTMLCharsetUTF8, resp.Header.Get(fiber.HeaderContentType))
@@ -74,6 +111,33 @@ func Test_Pprof_Subs(t *testing.T) {
 	}
 }
 
+func Test_Pprof_Subs_WithPrefix(t *testing.T) {
+	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+
+	app.Use(New(Config{Prefix: "/federated-fiber"}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("escaped")
+	})
+
+	subs := []string{
+		"cmdline", "profile", "symbol", "trace", "allocs", "block",
+		"goroutine", "heap", "mutex", "threadcreate",
+	}
+
+	for _, sub := range subs {
+		t.Run(sub, func(t *testing.T) {
+			target := "/federated-fiber/debug/pprof/" + sub
+			if sub == "profile" {
+				target += "?seconds=1"
+			}
+			resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, target, nil), 5000)
+			utils.AssertEqual(t, nil, err)
+			utils.AssertEqual(t, 200, resp.StatusCode)
+		})
+	}
+}
+
 func Test_Pprof_Other(t *testing.T) {
 	app := fiber.New(fiber.Config{DisableStartupMessage: true})
 
@@ -84,6 +148,20 @@ func Test_Pprof_Other(t *testing.T) {
 	})
 
 	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/debug/pprof/302", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 302, resp.StatusCode)
+}
+
+func Test_Pprof_Other_WithPrefix(t *testing.T) {
+	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+
+	app.Use(New(Config{Prefix: "/federated-fiber"}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("escaped")
+	})
+
+	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/federated-fiber/debug/pprof/302", nil))
 	utils.AssertEqual(t, nil, err)
 	utils.AssertEqual(t, 302, resp.StatusCode)
 }
@@ -101,6 +179,24 @@ func Test_Pprof_Next(t *testing.T) {
 	}))
 
 	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/debug/pprof/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, 404, resp.StatusCode)
+}
+
+// go test -run Test_Pprof_Next_WithPrefix
+func Test_Pprof_Next_WithPrefix(t *testing.T) {
+	t.Parallel()
+
+	app := fiber.New()
+
+	app.Use(New(Config{
+		Next: func(_ *fiber.Ctx) bool {
+			return true
+		},
+		Prefix: "/federated-fiber",
+	}))
+
+	resp, err := app.Test(httptest.NewRequest(fiber.MethodGet, "/federated-fiber/debug/pprof/", nil))
 	utils.AssertEqual(t, nil, err)
 	utils.AssertEqual(t, 404, resp.StatusCode)
 }
