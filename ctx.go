abad0d74094750a92c2fd1bea2c89bae0209e2d8
Leon Klingele <git@leonklingele.de>
2022-11-14 04:32:16 +0100

    ctx: use the same warning comment about enabling Config.EnableTrustedProxyCheck everywhere

--- ctx.go
+++ ctx.go
@@ -1040,7 +1040,7 @@ func (c *Ctx) Path(override ...string) string {
 }
 
 // Protocol contains the request protocol string: http or https for TLS requests.
-// Use Config.EnableTrustedProxyCheck to prevent header spoofing, in case when your app is behind the proxy.
+// Please use Config.EnableTrustedProxyCheck to prevent header spoofing, in case when your app is behind the proxy.
 func (c *Ctx) Protocol() string {
 	if c.fasthttp.IsTLS() {
 		return "https"
