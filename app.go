8952d170fe8bcaae9273b78455b6d7dfee4930d9
RW <rene@gofiber.io>
2022-11-23 09:05:31 +0100

    prepare release v2.40.1

--- app.go
+++ app.go
@@ -27,7 +27,7 @@ import (
 )
 
 // Version of current fiber package
-const Version = "2.40.0"
+const Version = "2.40.1"
 
 // Handler defines a function to serve HTTP requests.
 type Handler = func(*Ctx) error
