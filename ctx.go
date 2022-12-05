356448cb66ea04b28e6429d5be5948121b5d7c82
Angel ILIEV <a.v.iliev13@gmail.com>
2022-12-05 16:02:18 +0200

    Update docstring for FormValue() (#2262)
    Include the other places that are searched for the provided form value key

--- ctx.go
+++ ctx.go
@@ -553,6 +553,7 @@ func (c *Ctx) FormFile(key string) (*multipart.FileHeader, error) {
 }
 
 // FormValue returns the first value by key from a MultipartForm.
+// Search is performed in QueryArgs, PostArgs, MultipartForm and FormFile in this particular order.
 // Defaults to the empty string "" if the form value doesn't exist.
 // If a default value is given, it will return that value if the form value does not exist.
 // Returned value is only valid within the handler. Do not store any references.
