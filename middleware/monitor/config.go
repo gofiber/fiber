gofiber/fiber

commit 167a8b5e9421e0ab51fbf44c5621632f4a1a90c5

🚀 Feature: Add and apply more stricter golangci-lint linting rules (#2286)
* golangci-lint: add and apply more stricter linting rules

* github: drop security workflow now that we use gosec linter inside golangci-lint

* github: use official golangci-lint CI linter

* Add editorconfig and gitattributes file

middleware/monitor/config.go
@@ -41,36 +41,48 @@ type Config struct {
 	// ChartJsURL for specify ChartJS library  path or URL . also you can use relative path
 	//
 	// Optional. Default: https://cdn.jsdelivr.net/npm/chart.js@2.9/dist/Chart.bundle.min.js
-	ChartJsURL string
+	ChartJSURL string
 
 	index string
 }
 
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	Title:      defaultTitle,
 	Refresh:    defaultRefresh,
 	FontURL:    defaultFontURL,
-	ChartJsURL: defaultChartJsURL,
+	ChartJSURL: defaultChartJSURL,
 	CustomHead: defaultCustomHead,
 	APIOnly:    false,
 	Next:       nil,
-	index: newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJsURL,
-		defaultCustomHead}),
+	index: newIndex(viewBag{
+		defaultTitle,
+		defaultRefresh,
+		defaultFontURL,
+		defaultChartJSURL,
+		defaultCustomHead,
+	}),
 }
 
 func configDefault(config ...Config) Config {
 	// Users can change ConfigDefault.Title/Refresh which then
 	// become incompatible with ConfigDefault.index
-	if ConfigDefault.Title != defaultTitle || ConfigDefault.Refresh != defaultRefresh ||
-		ConfigDefault.FontURL != defaultFontURL || ConfigDefault.ChartJsURL != defaultChartJsURL ||
+	if ConfigDefault.Title != defaultTitle ||
+		ConfigDefault.Refresh != defaultRefresh ||
+		ConfigDefault.FontURL != defaultFontURL ||
+		ConfigDefault.ChartJSURL != defaultChartJSURL ||
 		ConfigDefault.CustomHead != defaultCustomHead {
-
 		if ConfigDefault.Refresh < minRefresh {
 			ConfigDefault.Refresh = minRefresh
 		}
 		// update default index with new default title/refresh
-		ConfigDefault.index = newIndex(viewBag{ConfigDefault.Title,
-			ConfigDefault.Refresh, ConfigDefault.FontURL, ConfigDefault.ChartJsURL, ConfigDefault.CustomHead})
+		ConfigDefault.index = newIndex(viewBag{
+			ConfigDefault.Title,
+			ConfigDefault.Refresh,
+			ConfigDefault.FontURL,
+			ConfigDefault.ChartJSURL,
+			ConfigDefault.CustomHead,
+		})
 	}
 
 	// Return default config if nothing provided
@@ -93,8 +105,8 @@ func configDefault(config ...Config) Config {
 		cfg.FontURL = defaultFontURL
 	}
 
-	if cfg.ChartJsURL == "" {
-		cfg.ChartJsURL = defaultChartJsURL
+	if cfg.ChartJSURL == "" {
+		cfg.ChartJSURL = defaultChartJSURL
 	}
 	if cfg.Refresh < minRefresh {
 		cfg.Refresh = minRefresh
@@ -112,8 +124,8 @@ func configDefault(config ...Config) Config {
 	cfg.index = newIndex(viewBag{
 		title:      cfg.Title,
 		refresh:    cfg.Refresh,
-		fontUrl:    cfg.FontURL,
-		chartJsUrl: cfg.ChartJsURL,
+		fontURL:    cfg.FontURL,
+		chartJSURL: cfg.ChartJSURL,
 		customHead: cfg.CustomHead,
 	})
 

middleware/monitor/config_test.go
@@ -18,11 +18,11 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set title", func(t *testing.T) {
@@ -35,11 +35,11 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, title, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{title, defaultRefresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{title, defaultRefresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set refresh less than default", func(t *testing.T) {
@@ -51,11 +51,11 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, minRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, minRefresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, minRefresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set refresh", func(t *testing.T) {
@@ -68,45 +68,45 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, refresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, refresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, refresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set font url", func(t *testing.T) {
 		t.Parallel()
-		fontUrl := "https://example.com"
+		fontURL := "https://example.com"
 		cfg := configDefault(Config{
-			FontURL: fontUrl,
+			FontURL: fontURL,
 		})
 
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
-		utils.AssertEqual(t, fontUrl, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, fontURL, cfg.FontURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, fontUrl, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, fontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set chart js url", func(t *testing.T) {
 		t.Parallel()
-		chartUrl := "http://example.com"
+		chartURL := "http://example.com"
 		cfg := configDefault(Config{
-			ChartJsURL: chartUrl,
+			ChartJSURL: chartURL,
 		})
 
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, chartUrl, cfg.ChartJsURL)
+		utils.AssertEqual(t, chartURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, chartUrl, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, chartURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set custom head", func(t *testing.T) {
@@ -119,11 +119,11 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, head, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJsURL, head}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJSURL, head}), cfg.index)
 	})
 
 	t.Run("set api only", func(t *testing.T) {
@@ -135,11 +135,11 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, true, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 
 	t.Run("set next", func(t *testing.T) {
@@ -154,10 +154,10 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJsURL, cfg.ChartJsURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, f(nil), cfg.Next(nil))
-		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJsURL, defaultCustomHead}), cfg.index)
+		utils.AssertEqual(t, newIndex(viewBag{defaultTitle, defaultRefresh, defaultFontURL, defaultChartJSURL, defaultCustomHead}), cfg.index)
 	})
 }

middleware/monitor/monitor_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
+
 	"github.com/valyala/fasthttp"
 )
 
@@ -61,6 +62,7 @@ func Test_Monitor_Html(t *testing.T) {
 		conf.Refresh.Milliseconds()-timeoutDiff)
 	utils.AssertEqual(t, true, bytes.Contains(buf, []byte(timeoutLine)))
 }
+
 func Test_Monitor_Html_CustomCodes(t *testing.T) {
 	t.Parallel()
 
@@ -82,8 +84,10 @@ func Test_Monitor_Html_CustomCodes(t *testing.T) {
 	utils.AssertEqual(t, true, bytes.Contains(buf, []byte(timeoutLine)))
 
 	// custom config
-	conf := Config{Title: "New " + defaultTitle, Refresh: defaultRefresh + time.Second,
-		ChartJsURL: "https://cdnjs.com/libraries/Chart.js",
+	conf := Config{
+		Title:      "New " + defaultTitle,
+		Refresh:    defaultRefresh + time.Second,
+		ChartJSURL: "https://cdnjs.com/libraries/Chart.js",
 		FontURL:    "/public/my-font.css",
 		CustomHead: `<style>body{background:#fff}</style>`,
 	}
@@ -136,7 +140,7 @@ func Benchmark_Monitor(b *testing.B) {
 	h := app.Handler()
 
 	fctx := &fasthttp.RequestCtx{}
-	fctx.Request.Header.SetMethod("GET")
+	fctx.Request.Header.SetMethod(fiber.MethodGet)
 	fctx.Request.SetRequestURI("/")
 	fctx.Request.Header.Set(fiber.HeaderAccept, fiber.MIMEApplicationJSON)
 

middleware/basicauth/config.go
@@ -53,6 +53,8 @@ type Config struct {
 }
 
 // ConfigDefault is the default config
+//
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	Next:            nil,
 	Users:           map[string]string{},

middleware/cache/config.go
@@ -1,7 +1,7 @@
 package cache
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -49,10 +49,10 @@ type Config struct {
 	// Default: an in memory store for this process only
 	Storage fiber.Storage
 
-	// Deprecated, use Storage instead
+	// Deprecated: Use Storage instead
 	Store fiber.Storage
 
-	// Deprecated, use KeyGenerator instead
+	// Deprecated: Use KeyGenerator instead
 	Key func(*fiber.Ctx) string
 
 	// allows you to store additional headers generated by next middlewares & handler
@@ -75,6 +75,8 @@ type Config struct {
 }
 
 // ConfigDefault is the default config
+//
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	Next:         nil,
 	Expiration:   1 * time.Minute,
@@ -102,11 +104,11 @@ func configDefault(config ...Config) Config {
 
 	// Set default values
 	if cfg.Store != nil {
-		fmt.Println("[CACHE] Store is deprecated, please use Storage")
+		log.Printf("[CACHE] Store is deprecated, please use Storage\n")
 		cfg.Storage = cfg.Store
 	}
 	if cfg.Key != nil {
-		fmt.Println("[CACHE] Key is deprecated, please use KeyGenerator")
+		log.Printf("[CACHE] Key is deprecated, please use KeyGenerator\n")
 		cfg.KeyGenerator = cfg.Key
 	}
 	if cfg.Next == nil {

middleware/compress/config.go
@@ -33,6 +33,8 @@ const (
 )
 
 // ConfigDefault is the default config
+//
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	Next:  nil,
 	Level: LevelDefault,

middleware/csrf/config.go
@@ -1,7 +1,7 @@
 package csrf
 
 import (
-	"fmt"
+	"log"
 	"net/textproto"
 	"strings"
 	"time"
@@ -80,13 +80,13 @@ type Config struct {
 	// Optional. Default: utils.UUID
 	KeyGenerator func() string
 
-	// Deprecated, please use Expiration
+	// Deprecated: Please use Expiration
 	CookieExpires time.Duration
 
-	// Deprecated, please use Cookie* related fields
+	// Deprecated: Please use Cookie* related fields
 	Cookie *fiber.Cookie
 
-	// Deprecated, please use KeyLookup
+	// Deprecated: Please use KeyLookup
 	TokenLookup string
 
 	// ErrorHandler is executed when an error is returned from fiber.Handler.
@@ -105,6 +105,8 @@ type Config struct {
 const HeaderName = "X-Csrf-Token"
 
 // ConfigDefault is the default config
+//
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	KeyLookup:      "header:" + HeaderName,
 	CookieName:     "csrf_",
@@ -116,7 +118,7 @@ var ConfigDefault = Config{
 }
 
 // default ErrorHandler that process return error from fiber.Handler
-var defaultErrorHandler = func(c *fiber.Ctx, err error) error {
+func defaultErrorHandler(_ *fiber.Ctx, _ error) error {
 	return fiber.ErrForbidden
 }
 
@@ -132,15 +134,15 @@ func configDefault(config ...Config) Config {
 
 	// Set default values
 	if cfg.TokenLookup != "" {
-		fmt.Println("[CSRF] TokenLookup is deprecated, please use KeyLookup")
+		log.Printf("[CSRF] TokenLookup is deprecated, please use KeyLookup\n")
 		cfg.KeyLookup = cfg.TokenLookup
 	}
 	if int(cfg.CookieExpires.Seconds()) > 0 {
-		fmt.Println("[CSRF] CookieExpires is deprecated, please use Expiration")
+		log.Printf("[CSRF] CookieExpires is deprecated, please use Expiration\n")
 		cfg.Expiration = cfg.CookieExpires
 	}
 	if cfg.Cookie != nil {
-		fmt.Println("[CSRF] Cookie is deprecated, please use Cookie* related fields")
+		log.Printf("[CSRF] Cookie is deprecated, please use Cookie* related fields\n")
 		if cfg.Cookie.Name != "" {
 			cfg.CookieName = cfg.Cookie.Name
 		}
@@ -178,7 +180,8 @@ func configDefault(config ...Config) Config {
 	// Generate the correct extractor to get the token from the correct location
 	selectors := strings.Split(cfg.KeyLookup, ":")
 
-	if len(selectors) != 2 {
+	const numParts = 2
+	if len(selectors) != numParts {
 		panic("[CSRF] KeyLookup must in the form of <source>:<key>")
 	}
 

middleware/encryptcookie/config.go
@@ -1,6 +1,8 @@
 package encryptcookie
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 // Config defines the config for middleware.
 type Config struct {
@@ -32,6 +34,8 @@ type Config struct {
 }
 
 // ConfigDefault is the default config
+//
+//nolint:gochecknoglobals // Using a global var is fine here
 var ConfigDefault = Config{
 	Next:      nil,
 	Except:    []string{"csrf_"},

commit ce44b44ff5d670df9fd5bd197ea1d35f256b445b

middleware/monitor: revert changes to exported field "ChartJSURL"
This is a breaking change introduced in 167a8b5e9421e0ab51fbf44c5621632f4a1a90c5.

middleware/monitor/config.go
@@ -41,7 +41,7 @@ type Config struct {
 	// ChartJsURL for specify ChartJS library  path or URL . also you can use relative path
 	//
 	// Optional. Default: https://cdn.jsdelivr.net/npm/chart.js@2.9/dist/Chart.bundle.min.js
-	ChartJSURL string
+	ChartJsURL string // TODO: Rename to "ChartJSURL" in v3
 
 	index string
 }
@@ -51,7 +51,7 @@ var ConfigDefault = Config{
 	Title:      defaultTitle,
 	Refresh:    defaultRefresh,
 	FontURL:    defaultFontURL,
-	ChartJSURL: defaultChartJSURL,
+	ChartJsURL: defaultChartJSURL,
 	CustomHead: defaultCustomHead,
 	APIOnly:    false,
 	Next:       nil,
@@ -70,7 +70,7 @@ func configDefault(config ...Config) Config {
 	if ConfigDefault.Title != defaultTitle ||
 		ConfigDefault.Refresh != defaultRefresh ||
 		ConfigDefault.FontURL != defaultFontURL ||
-		ConfigDefault.ChartJSURL != defaultChartJSURL ||
+		ConfigDefault.ChartJsURL != defaultChartJSURL ||
 		ConfigDefault.CustomHead != defaultCustomHead {
 		if ConfigDefault.Refresh < minRefresh {
 			ConfigDefault.Refresh = minRefresh
@@ -80,7 +80,7 @@ func configDefault(config ...Config) Config {
 			ConfigDefault.Title,
 			ConfigDefault.Refresh,
 			ConfigDefault.FontURL,
-			ConfigDefault.ChartJSURL,
+			ConfigDefault.ChartJsURL,
 			ConfigDefault.CustomHead,
 		})
 	}
@@ -105,8 +105,8 @@ func configDefault(config ...Config) Config {
 		cfg.FontURL = defaultFontURL
 	}
 
-	if cfg.ChartJSURL == "" {
-		cfg.ChartJSURL = defaultChartJSURL
+	if cfg.ChartJsURL == "" {
+		cfg.ChartJsURL = defaultChartJSURL
 	}
 	if cfg.Refresh < minRefresh {
 		cfg.Refresh = minRefresh
@@ -125,7 +125,7 @@ func configDefault(config ...Config) Config {
 		title:      cfg.Title,
 		refresh:    cfg.Refresh,
 		fontURL:    cfg.FontURL,
-		chartJSURL: cfg.ChartJSURL,
+		chartJSURL: cfg.ChartJsURL,
 		customHead: cfg.CustomHead,
 	})
 

middleware/monitor/config_test.go
@@ -18,7 +18,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -35,7 +35,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, title, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -51,7 +51,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, minRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -68,7 +68,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, refresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -85,7 +85,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, fontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -96,13 +96,13 @@ func Test_Config_Default(t *testing.T) {
 		t.Parallel()
 		chartURL := "http://example.com"
 		cfg := configDefault(Config{
-			ChartJSURL: chartURL,
+			ChartJsURL: chartURL,
 		})
 
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, chartURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, chartURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -119,7 +119,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, head, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -135,7 +135,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, true, cfg.APIOnly)
 		utils.AssertEqual(t, (func(*fiber.Ctx) bool)(nil), cfg.Next)
@@ -154,7 +154,7 @@ func Test_Config_Default(t *testing.T) {
 		utils.AssertEqual(t, defaultTitle, cfg.Title)
 		utils.AssertEqual(t, defaultRefresh, cfg.Refresh)
 		utils.AssertEqual(t, defaultFontURL, cfg.FontURL)
-		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJSURL)
+		utils.AssertEqual(t, defaultChartJSURL, cfg.ChartJsURL)
 		utils.AssertEqual(t, defaultCustomHead, cfg.CustomHead)
 		utils.AssertEqual(t, false, cfg.APIOnly)
 		utils.AssertEqual(t, f(nil), cfg.Next(nil))

middleware/monitor/monitor_test.go
@@ -87,7 +87,7 @@ func Test_Monitor_Html_CustomCodes(t *testing.T) {
 	conf := Config{
 		Title:      "New " + defaultTitle,
 		Refresh:    defaultRefresh + time.Second,
-		ChartJSURL: "https://cdnjs.com/libraries/Chart.js",
+		ChartJsURL: "https://cdnjs.com/libraries/Chart.js",
 		FontURL:    "/public/my-font.css",
 		CustomHead: `<style>body{background:#fff}</style>`,
 	}