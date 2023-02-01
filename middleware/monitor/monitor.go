gofiber/fiber

commit 167a8b5e9421e0ab51fbf44c5621632f4a1a90c5

🚀 Feature: Add and apply more stricter golangci-lint linting rules (#2286)
* golangci-lint: add and apply more stricter linting rules

* github: drop security workflow now that we use gosec linter inside golangci-lint

* github: use official golangci-lint CI linter

* Add editorconfig and gitattributes file

middleware/monitor/monitor.go
@@ -33,18 +33,20 @@ type statsOS struct {
 	Conns    int     `json:"conns"`
 }
 
+//nolint:gochecknoglobals // TODO: Do not use a global var here
 var (
-	monitPidCpu   atomic.Value
-	monitPidRam   atomic.Value
-	monitPidConns atomic.Value
+	monitPIDCPU   atomic.Value
+	monitPIDRAM   atomic.Value
+	monitPIDConns atomic.Value
 
-	monitOsCpu      atomic.Value
-	monitOsRam      atomic.Value
-	monitOsTotalRam atomic.Value
-	monitOsLoadAvg  atomic.Value
-	monitOsConns    atomic.Value
+	monitOSCPU      atomic.Value
+	monitOSRAM      atomic.Value
+	monitOSTotalRAM atomic.Value
+	monitOSLoadAvg  atomic.Value
+	monitOSConns    atomic.Value
 )
 
+//nolint:gochecknoglobals // TODO: Do not use a global var here
 var (
 	mutex sync.RWMutex
 	once  sync.Once
@@ -58,7 +60,7 @@ func New(config ...Config) fiber.Handler {
 
 	// Start routine to update statistics
 	once.Do(func() {
-		p, _ := process.NewProcess(int32(os.Getpid()))
+		p, _ := process.NewProcess(int32(os.Getpid())) //nolint:errcheck // TODO: Handle error
 
 		updateStatistics(p)
 
@@ -72,6 +74,7 @@ func New(config ...Config) fiber.Handler {
 	})
 
 	// Return new handler
+	//nolint:errcheck // Ignore the type-assertion errors
 	return func(c *fiber.Ctx) error {
 		// Don't execute middleware if Next returns true
 		if cfg.Next != nil && cfg.Next(c) {
@@ -83,15 +86,15 @@ func New(config ...Config) fiber.Handler {
 		}
 		if c.Get(fiber.HeaderAccept) == fiber.MIMEApplicationJSON || cfg.APIOnly {
 			mutex.Lock()
-			data.PID.CPU = monitPidCpu.Load().(float64)
-			data.PID.RAM = monitPidRam.Load().(uint64)
-			data.PID.Conns = monitPidConns.Load().(int)
+			data.PID.CPU, _ = monitPIDCPU.Load().(float64)
+			data.PID.RAM, _ = monitPIDRAM.Load().(uint64)
+			data.PID.Conns, _ = monitPIDConns.Load().(int)
 
-			data.OS.CPU = monitOsCpu.Load().(float64)
-			data.OS.RAM = monitOsRam.Load().(uint64)
-			data.OS.TotalRAM = monitOsTotalRam.Load().(uint64)
-			data.OS.LoadAvg = monitOsLoadAvg.Load().(float64)
-			data.OS.Conns = monitOsConns.Load().(int)
+			data.OS.CPU, _ = monitOSCPU.Load().(float64)
+			data.OS.RAM, _ = monitOSRAM.Load().(uint64)
+			data.OS.TotalRAM, _ = monitOSTotalRAM.Load().(uint64)
+			data.OS.LoadAvg, _ = monitOSLoadAvg.Load().(float64)
+			data.OS.Conns, _ = monitOSConns.Load().(int)
 			mutex.Unlock()
 			return c.Status(fiber.StatusOK).JSON(data)
 		}
@@ -101,29 +104,35 @@ func New(config ...Config) fiber.Handler {
 }
 
 func updateStatistics(p *process.Process) {
-	pidCpu, _ := p.CPUPercent()
-	monitPidCpu.Store(pidCpu / 10)
-
-	if osCpu, _ := cpu.Percent(0, false); len(osCpu) > 0 {
-		monitOsCpu.Store(osCpu[0])
+	pidCPU, err := p.CPUPercent()
+	if err != nil {
+		monitPIDCPU.Store(pidCPU / 10) //nolint:gomnd // TODO: Explain why we divide by 10 here
 	}
 
-	if pidMem, _ := p.MemoryInfo(); pidMem != nil {
-		monitPidRam.Store(pidMem.RSS)
+	if osCPU, err := cpu.Percent(0, false); err != nil && len(osCPU) > 0 {
+		monitOSCPU.Store(osCPU[0])
 	}
 
-	if osMem, _ := mem.VirtualMemory(); osMem != nil {
-		monitOsRam.Store(osMem.Used)
-		monitOsTotalRam.Store(osMem.Total)
+	if pidRAM, err := p.MemoryInfo(); err != nil && pidRAM != nil {
+		monitPIDRAM.Store(pidRAM.RSS)
 	}
 
-	if loadAvg, _ := load.Avg(); loadAvg != nil {
-		monitOsLoadAvg.Store(loadAvg.Load1)
+	if osRAM, err := mem.VirtualMemory(); err != nil && osRAM != nil {
+		monitOSRAM.Store(osRAM.Used)
+		monitOSTotalRAM.Store(osRAM.Total)
 	}
 
-	pidConns, _ := net.ConnectionsPid("tcp", p.Pid)
-	monitPidConns.Store(len(pidConns))
+	if loadAvg, err := load.Avg(); err != nil && loadAvg != nil {
+		monitOSLoadAvg.Store(loadAvg.Load1)
+	}
 
-	osConns, _ := net.Connections("tcp")
-	monitOsConns.Store(len(osConns))
+	pidConns, err := net.ConnectionsPid("tcp", p.Pid)
+	if err != nil {
+		monitPIDConns.Store(len(pidConns))
+	}
+
+	osConns, err := net.Connections("tcp")
+	if err != nil {
+		monitOSConns.Store(len(osConns))
+	}
 }

commit bc96c966db6fcf24e81a7335ea7039aea91d8f95

middleware/monitor: fix error checking
Fix the errorenous error checking introduced in 167a8b5e9421e0ab51fbf44c5621632f4a1a90c5.

middleware/monitor/monitor.go
@@ -105,34 +105,34 @@ func New(config ...Config) fiber.Handler {
 
 func updateStatistics(p *process.Process) {
 	pidCPU, err := p.CPUPercent()
-	if err != nil {
+	if err == nil {
 		monitPIDCPU.Store(pidCPU / 10) //nolint:gomnd // TODO: Explain why we divide by 10 here
 	}
 
-	if osCPU, err := cpu.Percent(0, false); err != nil && len(osCPU) > 0 {
+	if osCPU, err := cpu.Percent(0, false); err == nil && len(osCPU) > 0 {
 		monitOSCPU.Store(osCPU[0])
 	}
 
-	if pidRAM, err := p.MemoryInfo(); err != nil && pidRAM != nil {
+	if pidRAM, err := p.MemoryInfo(); err == nil && pidRAM != nil {
 		monitPIDRAM.Store(pidRAM.RSS)
 	}
 
-	if osRAM, err := mem.VirtualMemory(); err != nil && osRAM != nil {
+	if osRAM, err := mem.VirtualMemory(); err == nil && osRAM != nil {
 		monitOSRAM.Store(osRAM.Used)
 		monitOSTotalRAM.Store(osRAM.Total)
 	}
 
-	if loadAvg, err := load.Avg(); err != nil && loadAvg != nil {
+	if loadAvg, err := load.Avg(); err == nil && loadAvg != nil {
 		monitOSLoadAvg.Store(loadAvg.Load1)
 	}
 
 	pidConns, err := net.ConnectionsPid("tcp", p.Pid)
-	if err != nil {
+	if err == nil {
 		monitPIDConns.Store(len(pidConns))
 	}
 
 	osConns, err := net.Connections("tcp")
-	if err != nil {
+	if err == nil {
 		monitOSConns.Store(len(osConns))
 	}
 }