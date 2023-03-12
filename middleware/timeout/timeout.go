94e84f44d182e614459b0972f5617714ad364ade
hakankutluay <hakan.kutluay@yemeksepeti.com>
2023-03-12 12:32:02 +0300

    add original timeout middleware

--- middleware/timeout/timeout.go
+++ middleware/timeout/timeout.go
@@ -3,13 +3,49 @@ package timeout
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-// New implementation of timeout middleware. Set custom errors(context.DeadlineExceeded vs) for get fiber.ErrRequestTimeout response.
-func New(h fiber.Handler, t time.Duration, tErrs ...error) fiber.Handler {
+var once sync.Once
+
+// New wraps a handler and aborts the process of the handler if the timeout is reached
+func New(handler fiber.Handler, timeout time.Duration) fiber.Handler {
+	once.Do(func() {
+		fmt.Println("[Warning] timeout contains data race issues, not ready for production!")
+	})
+
+	if timeout <= 0 {
+		return handler
+	}
+
+	// logic is from fasthttp.TimeoutWithCodeHandler https://github.com/valyala/fasthttp/blob/master/server.go#L418
+	return func(ctx *fiber.Ctx) error {
+		ch := make(chan struct{}, 1)
+
+		go func() {
+			defer func() {
+				_ = recover()
+			}()
+			_ = handler(ctx)
+			ch <- struct{}{}
+		}()
+
+		select {
+		case <-ch:
+		case <-time.After(timeout):
+			return fiber.ErrRequestTimeout
+		}
+
+		return nil
+	}
+}
+
+// NewWithContext implementation of timeout middleware. Set custom errors(context.DeadlineExceeded vs) for get fiber.ErrRequestTimeout response.
+func NewWithContext(h fiber.Handler, t time.Duration, tErrs ...error) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		timeoutContext, cancel := context.WithTimeout(ctx.UserContext(), t)
 		defer cancel()
