2682b2df8a40b1c2d8ca47ba7e428f77047e2630
carmeloriolo <carmeloriolo93@gmail.com>
2023-04-15 19:50:42 +0200

    feat: split fiber.Storage interface into smaller ones

--- app.go
+++ app.go
@@ -41,22 +41,43 @@ type Map map[string]interface{}
 // Storage interface for communicating with different database/key-value
 // providers
 type Storage interface {
+	StorageGetter
+	StorageSetter
+	StorageDeleter
+	StorageResetter
+	StorageCloser
+}
+
+// StorageGetter interface exposing reading methods against storage
+type StorageGetter interface {
 	// Get gets the value for the given key.
 	// `nil, nil` is returned when the key does not exist
 	Get(key string) ([]byte, error)
+}
 
+// StorageSetter interface exposing writing methods against storage
+type StorageSetter interface {
 	// Set stores the given value for the given key along
 	// with an expiration value, 0 means no expiration.
 	// Empty key or value will be ignored without an error.
 	Set(key string, val []byte, exp time.Duration) error
+}
 
+// StorageDeleter interface exposing deletion methods against storage
+type StorageDeleter interface {
 	// Delete deletes the value for the given key.
 	// It returns no error if the storage does not contain the key,
 	Delete(key string) error
+}
 
+// StorageDeleter interface exposing reset methods against storage
+type StorageResetter interface {
 	// Reset resets the storage and delete all keys.
 	Reset() error
+}
 
+// StorageCloser interface exposing closing methods against storage
+type StorageCloser interface {
 	// Close closes the storage and will stop any running garbage
 	// collectors and open connections.
 	Close() error
