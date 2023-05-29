b5366813ce98a6ba5ce9f827821ccb16a3927117
Damian Hamill <damianham@gmail.com>
2023-05-29 12:06:33 -0500

    exclude the linter from failing on ineffective assign

--- ctx.go
+++ ctx.go
@@ -797,7 +797,7 @@ func (c *Ctx) JSON(data interface{}) error {
 	}
 	c.fasthttp.Response.SetBodyRaw(raw)
 	c.fasthttp.Response.Header.SetContentType(MIMEApplicationJSON)
-	raw = nil
+	raw = nil  //nolint:ineffassign
 	return nil
 }
 
