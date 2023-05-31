5d53263572159e8522eaeb2aba4e796fd5fd850e
Lucas Lemos <luk3skyw4lker1770@gmail.com>
2023-05-31 03:01:13 -0300

    :bug: fix: treat case for possible timer memory leak (#2488)

--- internal/gopsutil/common/sleep.go
+++ internal/gopsutil/common/sleep.go
@@ -11,6 +11,9 @@ func Sleep(ctx context.Context, interval time.Duration) error {
 	var timer = time.NewTimer(interval)
 	select {
 	case <-ctx.Done():
+		if !timer.Stop() {
+			<-timer.C
+		}
 		return ctx.Err()
 	case <-timer.C:
 		return nil
