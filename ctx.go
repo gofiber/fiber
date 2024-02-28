701c8c29f7f3dc05b94f83a017f6515a2f5f4ed8
Iliya <dozheiny@gmail.com>
2024-02-28 18:46:47 +0330

    remove pointer refer on Convert

--- ctx.go
+++ ctx.go
@@ -1734,15 +1734,15 @@ func (c *DefaultCtx) Bind() *Bind {
 }
 
 // Converts a string value to a specified type, handling errors and optional default values.
-func Convert[T any](value string, convertor func(string) (T, error), defaultValue ...T) (*T, error) {
+func Convert[T any](value string, convertor func(string) (T, error), defaultValue ...T) (T, error) {
 	converted, err := convertor(value)
 	if err != nil {
 		if len(defaultValue) > 0 {
-			return &defaultValue[0], nil
+			return defaultValue[0], nil
 		}
 
-		return nil, fmt.Errorf("failed to convert: %w", err)
+		return converted, fmt.Errorf("failed to convert: %w", err)
 	}
 
-	return &converted, nil
+	return converted, nil
 }
