a5111e8d052bcb81eec36e91bd8aaeb841dc90d1
Jason McNeil <sixcolors@mac.com>
2024-03-18 21:15:36 -0300

    test(middleware/csrf): parallel test

--- middleware/csrf/csrf_test.go
+++ middleware/csrf/csrf_test.go
@@ -878,6 +878,7 @@ func Test_CSRF_TrustedOrigins_InvalidOrigins(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			require.Panics(t, func() {
 				app := fiber.New()
 				app.Use(New(Config{
