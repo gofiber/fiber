a91045183fa65f012a7e95b83002636618c1a33a
René <rene@gofiber.io>
2024-03-28 10:07:01 +0100

    merge v2 in main(v3)

--- middleware/adaptor/adaptor_test.go
+++ middleware/adaptor/adaptor_test.go
@@ -244,7 +244,6 @@ func testFiberToHandlerFunc(t *testing.T, checkDefaultPort bool, app ...*fiber.A
 		require.Equal(t, expectedHost, c.Hostname(), "Host")
 		require.Equal(t, expectedHost, string(c.Request().Header.Host()), "Host")
 		require.Equal(t, "http://"+expectedHost, c.BaseURL(), "BaseURL")
-		require.Equal(t, expectedHost, string(c.Request().Header.Host()), "Host")
 
 		body := string(c.Body())
 		require.Equal(t, expectedBody, body, "Body")
