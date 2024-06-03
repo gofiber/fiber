046b4a9d4e65ce57cd56d42670ec64ac332a25b1
RW <rene@gofiber.io>
2024-06-03 08:37:22 +0200

    Adding a generator to generate the CTX interface (#3024)

--- middleware/idempotency/response.go
+++ middleware/idempotency/response.go
@@ -1,5 +1,8 @@
 package idempotency
 
+// response is a struct that represents the response of a request.
+// generation tool `go install github.com/tinylib/msgp@latest`
+//
 //go:generate msgp -o=response_msgp.go -io=false -unexported
 type response struct {
 	StatusCode int `msg:"sc"`
