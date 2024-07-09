292a7d1cdc4ce837ee4d9e05211ec895c2d0bc8b
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2024-07-08 22:48:30 -0400

    logger: Use Content-Length header for BytesReceived and BytesSent tags

--- middleware/logger/tags.go
+++ middleware/logger/tags.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/gofiber/fiber/v3"
@@ -87,13 +88,10 @@ func createTagMap(cfg *Config) map[string]LogFunc {
 			return output.Write(c.Body())
 		},
 		TagBytesReceived: func(output Buffer, c fiber.Ctx, _ *Data, _ string) (int, error) {
-			return appendInt(output, len(c.Request().Body()))
+			return output.WriteString(strconv.Itoa((c.Request().Header.ContentLength())))
 		},
 		TagBytesSent: func(output Buffer, c fiber.Ctx, _ *Data, _ string) (int, error) {
-			if c.Response().Header.ContentLength() < 0 {
-				return appendInt(output, 0)
-			}
-			return appendInt(output, len(c.Response().Body()))
+			return output.WriteString(strconv.Itoa((c.Response().Header.ContentLength())))
 		},
 		TagRoute: func(output Buffer, c fiber.Ctx, _ *Data, _ string) (int, error) {
 			return output.WriteString(c.Route().Path)
