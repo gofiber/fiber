4f88ab12c7b9bace48aeec05a88e596feff24a61
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-03-05 21:27:09 -0500

    Update keyauth_test.go

--- middleware/keyauth/keyauth_test.go
+++ middleware/keyauth/keyauth_test.go
@@ -561,7 +561,7 @@ func Test_TokenFromContext(t *testing.T) {
 
 	t.Run("invalid context type", func(t *testing.T) {
 		require.Panics(t, func() {
-			token := TokenFromContext("invalid")
+			_ := TokenFromContext("invalid")
 		})
 	})
 }
