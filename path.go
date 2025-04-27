dd150577ec1b909030768a220068a0e7f19c6a6e
Katsu <katzuyoru@gmail.com>
2025-04-27 21:20:10 +0530

    fix: lint issue

--- path.go
+++ path.go
@@ -279,13 +279,12 @@ func findNextParamPosition(pattern string) int {
 		// checking the found parameterStartChar is a cluster
 		i := nextParamPosition + 1
 		for i < len(pattern) {
-			if findNextNonEscapedCharsetPosition(pattern[i:i+1], parameterStartChars) == 0 {
-				nextParamPosition++
-				i++
-			} else {
+			if findNextNonEscapedCharsetPosition(pattern[i:i+1], parameterStartChars) != 0 {
 				// It was a single parameter start char or end of cluster
 				break
 			}
+			nextParamPosition++
+			i++
 		}
 	}
 	return nextParamPosition
