037b5ff9e87c291b653d16cd47ef98db987fee10
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-06-05 00:37:49 -0400

    Update ctx_test.go

--- ctx_test.go
+++ ctx_test.go
@@ -676,7 +676,7 @@ func Test_Ctx_BodyParser_InvalidRequestData(t *testing.T) {
 	err := c.BodyParser(subject)
 
 	utils.AssertEqual(t, true, nil != err)
-	utils.AssertEqual(t, true, errors.Is(err, errInvalidPath))
+	utils.AssertEqual(t, "failed to decode: schema: invalid path", fmt.Sprintf("%v", err))
 
 	c.Request().Reset()
 	c.Request().Header.SetContentType(MIMEApplicationForm)
@@ -687,7 +687,7 @@ func Test_Ctx_BodyParser_InvalidRequestData(t *testing.T) {
 	err = c.BodyParser(subject)
 
 	utils.AssertEqual(t, true, nil != err)
-	utils.AssertEqual(t, true, errors.Is(err, errIndexTooLarge))
+	utils.AssertEqual(t, "failed to decode: schema: index exceeds parser limit", fmt.Sprintf("%v", err))
 }
 
 func Test_Ctx_ParamParser(t *testing.T) {
