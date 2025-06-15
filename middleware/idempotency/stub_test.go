26150d51353c081f7513fd16e0ea01c7e9b2f6d4
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-06-15 00:17:59 -0400

    Consolidate idempotency tests

---
+++ middleware/idempotency/stub_test.go
@@ -0,0 +1,62 @@
+package idempotency
+
+import "time"
+
+// stubLock implements Locker for testing purposes.
+type stubLock struct {
+	lockErr   error
+	unlockErr error
+	afterLock func()
+}
+
+func (s *stubLock) Lock(string) error {
+	if s.afterLock != nil {
+		s.afterLock()
+	}
+	return s.lockErr
+}
+func (s *stubLock) Unlock(string) error { return s.unlockErr }
+
+// stubStorage implements fiber.Storage for testing.
+type stubStorage struct {
+	data     map[string][]byte
+	getErr   error
+	setErr   error
+	setCount int
+}
+
+func (s *stubStorage) Get(key string) ([]byte, error) {
+	if s.getErr != nil {
+		return nil, s.getErr
+	}
+	if s.data == nil {
+		return nil, nil
+	}
+	return s.data[key], nil
+}
+
+func (s *stubStorage) Set(key string, val []byte, _ time.Duration) error {
+	if s.setErr != nil {
+		return s.setErr
+	}
+	if s.data == nil {
+		s.data = make(map[string][]byte)
+	}
+	s.data[key] = val
+	s.setCount++
+	return nil
+}
+
+func (s *stubStorage) Delete(key string) error {
+	if s.data != nil {
+		delete(s.data, key)
+	}
+	return nil
+}
+
+func (s *stubStorage) Reset() error {
+	s.data = make(map[string][]byte)
+	return nil
+}
+
+func (s *stubStorage) Close() error { return nil }
