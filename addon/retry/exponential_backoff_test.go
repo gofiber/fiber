a7a5657bb2752e455a201a441f23b4729f49007b
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-06-17 22:58:04 -0400

    Update exponential_backoff_test.go

--- addon/retry/exponential_backoff_test.go
+++ addon/retry/exponential_backoff_test.go
@@ -122,10 +122,10 @@ func Test_ExponentialBackoff_Next(t *testing.T) {
 }
 
 func Test_ExponentialBackoff_NextRandFailure(t *testing.T) {
-    // Backup original reader and restore at the end
-    original := rand.Reader
-    defer func() { rand.Reader = original }()
-    rand.Reader = failingReader{}
+	// Backup original reader and restore at the end
+	original := rand.Reader
+	defer func() { rand.Reader = original }()
+	rand.Reader = failingReader{}
 
 	expBackoff := &ExponentialBackoff{
 		InitialInterval: 1 * time.Second,
