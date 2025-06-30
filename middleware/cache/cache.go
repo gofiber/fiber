bdd9e22bd7c972524fbe278bbbbae0f0c4ef231c
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-06-30 03:04:35 -0400

    🐛 bug: Fix Cache middleware handling of Age (#3547)
    * Revert benchmark loop changes
    
    * test: cover Age header parsing
    
    * generate msgp files
    
    * Update cache.go
    
    * Cast result
    
    * Update cache.go

--- middleware/cache/cache.go
+++ middleware/cache/cache.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/utils/v2"
+	"github.com/valyala/fasthttp"
 )
 
 // timestampUpdatePeriod is the period which is used to check the cache expiration.
@@ -59,7 +60,6 @@ var cacheableStatusCodes = map[int]bool{
 	fiber.StatusMethodNotAllowed:            true,
 	fiber.StatusGone:                        true,
 	fiber.StatusRequestURITooLong:           true,
-	fiber.StatusTeapot:                      true,
 	fiber.StatusNotImplemented:              true,
 }
 
@@ -168,8 +168,9 @@ func New(config ...Config) fiber.Handler {
 					c.Set(fiber.HeaderCacheControl, "public, max-age="+maxAge)
 				}
 
-				// RFC-compliant Age header
-				age := strconv.FormatUint(e.ttl-(e.exp-ts), 10)
+				// RFC-compliant Age header (RFC 9111)
+				resident := e.ttl - (e.exp - ts)
+				age := strconv.FormatUint(e.age+resident, 10)
 				c.Response().Header.Set(fiber.HeaderAge, age)
 
 				c.Set(cfg.CacheHeader, cacheHit)
@@ -239,9 +240,15 @@ func New(config ...Config) fiber.Handler {
 		e.ctype = utils.CopyBytes(c.Response().Header.ContentType())
 		e.cencoding = utils.CopyBytes(c.Response().Header.Peek(fiber.HeaderContentEncoding))
 
-		if len(c.Response().Header.Peek(fiber.HeaderAge)) == 0 {
+		ageVal := uint64(0)
+		if b := c.Response().Header.Peek(fiber.HeaderAge); len(b) > 0 {
+			if v, err := fasthttp.ParseUint(b); err == nil {
+				ageVal = uint64(v) //nolint:gosec //Not a concern
+			}
+		} else {
 			c.Response().Header.Set(fiber.HeaderAge, "0")
 		}
+		e.age = ageVal
 
 		// Store all response headers
 		// (more: https://datatracker.ietf.org/doc/html/rfc2616#section-13.5.1)
