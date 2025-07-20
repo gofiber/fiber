a7871b083facf37e405f470f3e12a53c624ec81d
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-07-20 12:35:32 -0400

    test: expand retry config coverage

--- addon/retry/config_test.go
+++ addon/retry/config_test.go
@@ -40,3 +40,30 @@ func TestConfigDefault_CustomInitialInterval(t *testing.T) {
 	require.Equal(t, 5*time.Second, cfg.currentInterval)
 	require.Equal(t, 5*time.Second, cfg.InitialInterval)
 }
+
+func TestConfigDefault_CustomCurrentInterval(t *testing.T) {
+	t.Parallel()
+
+	cfg := configDefault(Config{currentInterval: 3 * time.Second})
+
+	require.Equal(t, 3*time.Second, cfg.currentInterval)
+	require.Equal(t, DefaultConfig.InitialInterval, cfg.InitialInterval)
+}
+
+func TestConfigDefault_CurrentIntervalAndInitialDiffer(t *testing.T) {
+	t.Parallel()
+
+	cfg := configDefault(Config{InitialInterval: 5 * time.Second, currentInterval: 3 * time.Second})
+
+	require.Equal(t, 5*time.Second, cfg.InitialInterval)
+	require.Equal(t, 3*time.Second, cfg.currentInterval)
+}
+
+func TestNewExponentialBackoff_Config(t *testing.T) {
+	t.Parallel()
+
+	backoff := NewExponentialBackoff(Config{InitialInterval: 4 * time.Second})
+
+	require.Equal(t, 4*time.Second, backoff.InitialInterval)
+	require.Equal(t, 4*time.Second, backoff.currentInterval)
+}
