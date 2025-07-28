5bc44677ce4fc13f735199622b3308bf232b8465
RW <rene@gofiber.io>
2025-07-28 13:28:21 +0200

    Fix race in cookie tests (#3629)

--- ctx_test.go
+++ ctx_test.go
@@ -1201,7 +1201,6 @@ func Test_Ctx_Cookie_StrictPartitioned(t *testing.T) {
 func Test_Ctx_Cookie_SameSite_CaseInsensitive(t *testing.T) {
 	t.Parallel()
 	app := New()
-	c := app.AcquireCtx(&fasthttp.RequestCtx{})
 
 	tests := []struct {
 		name     string
@@ -1240,6 +1239,9 @@ func Test_Ctx_Cookie_SameSite_CaseInsensitive(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
+			c := app.AcquireCtx(&fasthttp.RequestCtx{})
+			defer app.ReleaseCtx(c)
+
 			// Reset response
 			c.Response().Reset()
 
