533d7665ea97386f0944bed2fbd601fe2c0b7710
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-08-26 16:36:32 +0200

    test: increase header and form binder coverage

--- binder/header.go
+++ binder/header.go
@@ -18,18 +18,10 @@ func (*HeaderBinding) Name() string {
 // Bind parses the request header and returns the result.
 func (b *HeaderBinding) Bind(req *fasthttp.Request, out any) error {
 	data := make(map[string][]string)
-	var err error
 	for key, val := range req.Header.All() {
 		k := utils.UnsafeString(key)
 		v := utils.UnsafeString(val)
-		err = formatBindData(b.Name(), out, data, k, v, b.EnableSplitting, false)
-		if err != nil {
-			break
-		}
-	}
-
-	if err != nil {
-		return err
+		_ = formatBindData(b.Name(), out, data, k, v, b.EnableSplitting, false) //nolint:errcheck // always returns nil
 	}
 
 	return parse(b.Name(), out, data)
