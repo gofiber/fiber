82a84855deaced9b0d0a4322dc4f0ddc23360d3c
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-08-28 09:14:15 +0200

    🧹 chore: Expand Binder tests coverage (#3714)

--- binder/resp_header.go
+++ binder/resp_header.go
@@ -18,19 +18,13 @@ func (*RespHeaderBinding) Name() string {
 // Bind parses the response header and returns the result.
 func (b *RespHeaderBinding) Bind(resp *fasthttp.Response, out any) error {
 	data := make(map[string][]string)
-	var err error
 
 	for key, val := range resp.Header.All() {
 		k := utils.UnsafeString(key)
 		v := utils.UnsafeString(val)
-		err = formatBindData(b.Name(), out, data, k, v, b.EnableSplitting, false)
-		if err != nil {
-			break
+		if err := formatBindData(b.Name(), out, data, k, v, b.EnableSplitting, false); err != nil {
+			return err
 		}
-	}
-
-	if err != nil {
-		return err
 	}
 
 	return parse(b.Name(), out, data)
