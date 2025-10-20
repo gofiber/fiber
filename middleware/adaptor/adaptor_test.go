225d3b5fdba3e4b31a1ab178f37be517ade3b2cc
RW <rene@gofiber.io>
2025-10-20 13:20:16 +0200

    Fix fatal error calls in adaptor_test.go

--- middleware/adaptor/adaptor_test.go
+++ middleware/adaptor/adaptor_test.go
@@ -164,7 +164,7 @@ func Test_HTTPHandler_Flush(t *testing.T) {
 		fmt.Fprintf(w, "request body is ")
 		flusher, ok := w.(http.Flusher)
 		if !ok {
-			t.Fatalf("w does not implement http.Flusher")
+			t.Fatal("w does not implement http.Flusher")
 		}
 		flusher.Flush()
 		fmt.Fprintf(w, "%q", body)
@@ -212,7 +212,7 @@ func Test_HTTPHandler_Flush_App_Test(t *testing.T) {
 	app.Get("/", HTTPHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		flusher, ok := w.(http.Flusher)
 		if !ok {
-			t.Fatalf("w does not implement http.Flusher")
+			t.Fatal("w does not implement http.Flusher")
 		}
 		w.WriteHeader(fiber.StatusOK)
 		fmt.Fprintf(w, "Hello ")
