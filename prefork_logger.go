b32cf4f4e8d7cfc72e00cb39224b09197b246dd0
René <rene@gofiber.io>
2026-01-28 09:10:19 +0100

    feat(prefork): add integration tests and logger adapter for prefork functionality

---
+++ prefork_logger.go
@@ -0,0 +1,11 @@
+package fiber
+
+import "github.com/gofiber/fiber/v3/log"
+
+// preforkLogger adapts fiber's logger to the fasthttp prefork Logger interface.
+type preforkLogger struct{}
+
+// Printf implements the fasthttp prefork Logger interface.
+func (preforkLogger) Printf(format string, args ...any) {
+	log.Infof(format, args...)
+}
